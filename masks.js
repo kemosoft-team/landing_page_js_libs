db4bb6c09e18a492c96115c7fc3894679745acff
vitoralmeida <vitor.almeida.101@ufrn.edu.br>
2023-04-06 11:37:40 -0300

    feat: celular mask

--- masks.js
+++ masks.js
@@ -1,4 +1,6 @@
 var cpfInput = document.querySelector('[data-label="CPF"]');
+var celularInput = document.querySelector('[data-label="Celular"]');
+
     
     cpfInput.addEventListener("input", function() {
         
@@ -10,4 +12,16 @@ var cpfInput = document.querySelector('[data-label="CPF"]');
             cpf = cpf.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
         
     cpfInput.value = cpf;
-});
+    });
+
+
+    celularInput.addEventListener("input", function() {
+
+    var celular = celularInput.value;
+        celular = celular.replace(/\D/g, "");
+        celular = celular.substring(0, 11);
+        celular = celular.replace(/(\d{2})(\d)/, "($1) $2");
+        celular = celular.replace(/(\d{1})(\d{4})(\d{4})$/, "$1 $2-$3");
+
+    celularInput.value = celular;
+    });
