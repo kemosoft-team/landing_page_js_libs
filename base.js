13d06180a6be5e78abff0bd15c42e0d427c69925
vitoralmeida <vitor.almeida.101@ufrn.edu.br>
2023-04-13 13:54:26 -0300

    refactor: base to new cookie flow

--- base.js
+++ base.js
@@ -1,6 +1,6 @@
 
-let baseUrl = 'https://api.consigmais.com.br/lp/main/v2';
-let generalStepUrl = 'https://peach15985913.kemobuilder.site/';
+let APIAPIbaseURL = 'https://api2.kemosoft.com.br/api:lp';
+let generalStepUrl = 'https://infos.faz.vc/';
 
 function redirectToNextStep(nextStep){
   console.log("redirectToNextStep");
@@ -49,7 +49,7 @@ function showToast(text) {
 //get Token Status
 function getTokenStatus(){
 
-  axios.post('https://api.consigmais.com.br/lp/main/v2/getTokenData', {}, {
+  axios.post(APIbaseURL+'/lead-status', {}, {
     headers: {
       'Authorization': `Bearer ${getCookie('tkn')}`
     }})
@@ -95,29 +95,29 @@ async function getBanks(){
 }
 
 //obtem os parametros do afiliado oriondos dos cookies
-function captureAffiliateData(){
+// function captureAffiliateData(){
 
-  if (document.cookie) {
+//   if (document.cookie) {
 
-      let affiliateData = {
-          affiliateCode: getCookie('af') || null,
-          source: getCookie('source') || null,
-          productId: getCookie('pid') || null,
-          vendorId: getCookie('vid') || null,
-          offerId: getCookie('oid') || null,
-          clickId : getCookie('cid') || null,
-          pixelId: getCookie('afx') || null,
-          gtmId: getCookie('afgtm') || null,
-          latDays: getCookie('latd') || null,
-          brandId : getCookie('bid') || null,
-          nextStep : getCookie('nxstp') || null,
-          token: getCookie('tkn') || null,
-          rawUri: window.location.search
-      };
+//       let affiliateData = {
+//           affiliateCode: getCookie('af') || null,
+//           source: getCookie('source') || null,
+//           productId: getCookie('pid') || null,
+//           vendorId: getCookie('vid') || null,
+//           offerId: getCookie('oid') || null,
+//           clickId : getCookie('cid') || null,
+//           pixelId: getCookie('afx') || null,
+//           gtmId: getCookie('afgtm') || null,
+//           latDays: getCookie('latd') || null,
+//           brandId : getCookie('bid') || null,
+//           nextStep : getCookie('nxstp') || null,
+//           token: getCookie('tkn') || null,
+//           rawUri: window.location.search
+//       };
   
-      return affiliateData;
-  }
-}
+//       return affiliateData;
+//   }
+// }
 
 // Obtem infos do ip do cliente
 function getIpInfo(){
@@ -134,59 +134,49 @@ function getIpInfo(){
 
 function setCookies(latDays){
 
-  const ipInfoData = {
-    userIpInfo : getIpInfo(),
-    urlOrigin : window.location.href,
-  }; 
-
-    const urlParams = new URLSearchParams(window.location.search);
-    const paramsArray = [];
-
-      for (const [key, value] of urlParams.entries()) {
-          paramsArray.push({ name: key, value: value });
-      }
-
-    var expirationDays = latDays || 7;
-    var expirationDate = new Date();
-    
-    expirationDate.setTime(expirationDate.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
-
-    paramsArray.forEach(param => {
-      document.cookie = param.name + "=" + param.value + "; expires=" + expirationDate.toUTCString() + "; path=/;";
-    });
-
-    document.cookie = "client_origin="+JSON.stringify(ipInfoData)+"; expires=" + expirationDate.toUTCString() + "; path=/;";
-    captureAffiliateData();
-
-    }
-
-//Regiter Customer
-async function registerCustomer(name, birth, federalId, phone, email){
-  
-  axios.post(baseUrl+'/registerCustomer', {
-    "name": name,
-    "birth": birth,
-    "federalId": federalId,
-    "phone": phone,
-    "email": email,
-    "useTerms":true,
-    "dataPrivacy":true,
-    "dataSearchAllowed":true,
-    "affiliateData" : captureAffiliateData()
-
-  })
+  axios.get('https://ipinfo.io/json')
   .then(function (response) {
-    return response;
-    // redirectToNextStep(response.data.nextStep);
+            
+      const ip = response.data.ip;
+      const hostname = response.data.hostname;
+      const city = response.data.city;
+      const region = response.data.region;
+      const country = response.data.country;
+      const loc = response.data.loc;
+      const org = response.data.org;
+
+      let ipinfo = {
+        ip: ip || null,
+        hostname: hostname || null,
+        city: city || null,
+        region: region || null,
+        country: country || null,
+        loc: loc || null,
+        org: org || null,
+    };
+  
+      const urlParams = new URLSearchParams(window.location.search);
+  
+        for (const [key, value] of urlParams.entries()) {
+            ipinfo[key] = value;
+        }
+  
+      var expirationDays = latDays || 7;
+      var expirationDate = new Date();
+      
+      expirationDate.setTime(expirationDate.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
+
+      document.cookie = "client_origin="+encodeURIComponent(JSON.stringify(ipinfo))+"; expires=" + expirationDate.toUTCString() + "; path=/;";
   })
   .catch(function (error) {
-      showToast(error.response.data.message);
+      console.log(error);
   }); 
-}
+    }
+
 
 async function getByZipCodeInfo(zipcode){
 
-  axios.post(baseUrl+'/getZipcodeInfo', {
+  axios.post(APIbaseURL+'/getZipcodeInfo', {
     zipcode: zipcode,
   },
   {
@@ -218,7 +208,7 @@ function setAddressInfo(obj){
 //registerCustomer
 async function registerCustomer(name, birth, federalId, phone, email){
   
-  axios.post(baseUrl+'/registerCustomer', {
+  axios.post(APIbaseURL+'/offer-request-start', {
     "name": name,
     "birth": birth,
     "federalId": federalId,
@@ -227,10 +217,8 @@ async function registerCustomer(name, birth, federalId, phone, email){
     "useTerms":true,
     "dataPrivacy":true,
     "dataSearchAllowed":true,
-    "affiliateData" : captureAffiliateData()
   })
   .then((response) => {
-    console.log(response);
     handleSetToken(response.data.token);
     redirectToNextStep(response.data.nextStep);
   })
@@ -242,7 +230,7 @@ async function registerCustomer(name, birth, federalId, phone, email){
   //registerCustomerAccount
   async function registerCustomerAddress(zipcode, address, addressNumber, state, district, city) {
 
-    axios.post(baseUrl+'/registerCustomerInfos', {
+    axios.post(APIbaseURL+'/offer-request-infos', {
       zipcode: zipcode,
       address: address, 
       addressNumber: addressNumber, 
@@ -256,7 +244,6 @@ async function registerCustomer(name, birth, federalId, phone, email){
         'Authorization': `Bearer ${getCookie('tkn')}`
       }})
     .then((response) => {
-      console.log(response);
       redirectToNextStep(response.data.nextStep);
     })
     .catch(function (error) {
@@ -268,7 +255,7 @@ async function registerCustomer(name, birth, federalId, phone, email){
 //registerCustomerAccount
 async function registerCustomerAccount(agency, bank, account, verifyDigit, accountType) {
 
-  axios.post(baseUrl+'/registerCustomerInfos', {
+  axios.post(APIbaseURL+'/offer-request-infos', {
     branchNo: agency,
     bankId: bank,
     acctNo: `${account}-${verifyDigit}`,
@@ -293,7 +280,7 @@ async function registerCustomerAccount(agency, bank, account, verifyDigit, accou
   // registerCustomerAccount
 async function registerCustomerDocs(docNumber, docType, issueState, motherName) {
 
-  axios.post(baseUrl+'/registerCustomerInfos', {
+  axios.post(APIbaseURL+'/offer-request-infos', {
     docNumber: docNumber,
     docType: docType,
     docState: issueState,
@@ -391,7 +378,7 @@ function redirectTo(link, path){
  //get status pelo token
  if (getCookie('tkn') != null) { getTokenStatus() };
  //verifica se tem cookie, caso positivo prepara o affiliateData senao pega da url e prepara o affiliateData
- document.cookie ? captureAffiliateData() : setCookies()
+ setCookies();
 
 
 
