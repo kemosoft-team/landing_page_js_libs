114b089eeb7d1c70cb9b2ceb2f1be3a9822765e2
Vitor Almeida <vitorarj@ufrn.edu.br>
2023-04-26 12:09:57 -0300

    fix: brandId

--- creatives.js
+++ creatives.js
@@ -3,7 +3,17 @@ var url = new URL(url_params);
 let offer = url.searchParams.get("id"); 
 
 
-const tr = '<tr class="brz-table__tr"><th class="brz-table__th brz-table__aside"><div class="brz-table__th--btn">'+
+
+
+axios.post('https://api2.kemosoft.com.br/api:workflow/creatives', {
+    offerId: offer
+})
+.then(function (response) {
+    console.log(response);
+
+    response.creatives.forEach(element => {
+
+        const tr = '<tr class="brz-table__tr"><th class="brz-table__th brz-table__aside"><div class="brz-table__th--btn">'+
             '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 24 24" xml:'+
             'space="preserve" class="brz-icon-svg align-[initial]" data-type="outline" data-name="zoom-in"><g transform="translate(0, 0)" class="nc-icon-wrapper" fill="none">'+
             '<line data-color="color-2" fill="none" stroke="currentColor" vector-effect="non-scaling-stroke" stroke-linecap="square" stroke-miterlimit="10" x1="22" y1="22" x2='+
@@ -13,19 +23,30 @@ const tr = '<tr class="brz-table__tr"><th class="brz-table__th brz-table__aside"
             'vector-effect="non-scaling-stroke" stroke-linecap="square" stroke-miterlimit="10" x1="13" y1="10" x2="7" y2="10" stroke-linejoin="miter"></line></g></svg>'+
             '<span class="brz-span brz-text__editor"></span></div></th><td class="brz-table__td"><div class="brz-d-xs-flex brz-flex-xs-column"><div class="brz-css-ibzov brz-wrapper">'+
             '<div class="brz-rich-text brz-rich-text__custom brz-css-jenuz" data-custom-id="gvvamjkarfcftdlunfltalsbziwqxhkywics"><div><p data-uniq-id="rgtyl" data-generated-css='+
-            '"brz-css-wcplh" class="brz-css-oafdp">5581_Display_1-Ao-Mes_13-04-23_1080x1920.jpg"</p></div></div></div></div></td><td class="brz-table__td"><div class="brz-d-xs-flex '+
+            '"brz-css-wcplh" class="brz-css-oafdp">'+element.file_name+'</p></div></div></div></div></td><td class="brz-table__td"><div class="brz-d-xs-flex '+
             'brz-flex-xs-column"><div class="brz-css-ibzov brz-wrapper"><div class="brz-rich-text brz-rich-text__custom brz-css-jenuz" data-custom-id="fnlzivzmfpzxrfqpebrbczgussmdgqmkqxee">'+
-            '<div><p data-uniq-id="odsfb" data-generated-css="brz-css-nvodp" class="brz-css-khrpx">1080x1920</p></div></div></div></div></td><td class="brz-table__td">'+
+            '<div><p data-uniq-id="odsfb" data-generated-css="brz-css-nvodp" class="brz-css-khrpx">'+element.width+'x'+element.height+'</p></div></div></div></div></td><td class="brz-table__td">'+
             '<div class="brz-d-xs-flex brz-flex-xs-column"><div class="brz-css-ibzov brz-wrapper"><div class="brz-rich-text brz-rich-text__custom brz-css-jenuz" data-custom-id='+
-            '"awezwluvfavtxffdorwbasypmczspowkoqcy"><div><p data-uniq-id="tdinl" data-generated-css="brz-css-fjvte" class="brz-css-fwbbg">jpeg</p></div></div></div></div></td>'+
+            '"awezwluvfavtxffdorwbasypmczspowkoqcy"><div><p data-uniq-id="tdinl" data-generated-css="brz-css-fjvte" class="brz-css-fwbbg">'+element.type+'</p></div></div></div></div></td>'+
             '<td class="brz-table__td"><div class="brz-d-xs-flex brz-flex-xs-column"><div class="brz-wrapper-clone brz-css-xeowh"><div class="brz-d-xs-flex brz-flex-xs-wrap '+
-            'brz-css-lfxda"><div class="brz-wrapper-clone__item brz-css-ygmfe" id="dxpklhwdnjzqsvpxefarvciljzprpojvsuxa"><div class="brz-icon__container" data-custom-id="dxpklhwdnjzqsvpxefarvciljzprpojvsuxa"><span class="brz-icon brz-span brz-css-hcefj brz-css-wjkal"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 24 24" xml:space="preserve" class="brz-icon-svg align-[initial]" data-type="glyph" data-name="hit-down"><g class="nc-icon-wrapper" fill="currentColor"><path data-color="color-2" fill="currentColor" d="M24,23c0,0.552-0.448,1-1,1H1c-0.552,0-1-0.448-1-1c0-0.552,0.448-1,1-1h22 C23.552,22,24,22.448,24,23z"></path> <path fill="currentColor" d="M5,9l7,9l7-9h-6V1c0-0.553-0.447-1-1-1c-0.553,0-1,0.447-1,1v8H5z"></path></g></svg></span></div></div></div></div></div></td></tr>';
+            'brz-css-lfxda"><div class="brz-wrapper-clone__item brz-css-ygmfe" id="dxpklhwdnjzqsvpxefarvciljzprpojvsuxa"><div class="brz-icon__container" data-custom-id="dxpklhwdnjzqsvpxefarvciljzprpojvsuxa">'+
+            '<a class="brz-a" href="https:criativos.kemobuilder.site/" data-brz-link-type="external"><span class="brz-icon brz-span brz-css-hcefj brz-css-wjkal"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 24 24" xml:'+
+            'space="preserve" class="brz-icon-svg align-[initial]" data-type="glyph" data-name="hit-down"><g class="nc-icon-wrapper" fill="currentColor"><path data-color="color-2" fill="currentColor" d="M24,23c0,'+
+            '0.552-0.448,1-1,1H1c-0.552,0-1-0.448-1-1c0-0.552,0.448-1,1-1h22 C23.552,22,24,22.448,24,23z"></path> <path fill="currentColor" d="M5,9l7,9l7-9h-6V1c0-0.553-0.447-1-1-1c-0.553,0-1,0.447-1,1v8H5z"></path>'+
+            '</g></svg></span></div></div></div></div></div></td></tr>';
 
-axios.post('https://api2.kemosoft.com.br/api:workflow/creatives', {
-    offerId: offer
-})
-.then(function (response) {
-    console.log(response);
+            const temp = document.createElement('tbody');
+            temp.innerHTML = tr;
+
+            const trElement = temp.firstChild;
+
+            const tbody = document.querySelector('tbody');
+            tbody.appendChild(trElement);
+
+        
+    });
+
+
 
 
 })
@@ -34,10 +55,4 @@ axios.post('https://api2.kemosoft.com.br/api:workflow/creatives', {
 }); 
 
 
-const temp = document.createElement('tbody');
-temp.innerHTML = tr;
 
-const trElement = temp.firstChild;
-
-const tbody = document.querySelector('tbody');
-tbody.appendChild(trElement);
