5190f2362ada187b022d4d5d6f4b2c4a6ff50e7b
Vitor Almeida <vitorarj@ufrn.edu.br>
2023-04-26 20:56:02 -0300

    feat: mask whatsapp

--- sendLinkWhatsapp.js
+++ sendLinkWhatsapp.js
@@ -11,9 +11,17 @@ axios.post('https://api2.kemosoft.com.br/api:workflow/sendWhatsappMessage', {
     link: link
 })
 .then(function (response) {
+    console.log(response);
 })
 .catch(function (error) {
     console.log(error);
 }); 
 
 }
+
+var celularInput = document.querySelector('[data-label="Whatsapp"]');
+
+celularInput.addEventListener("input", (function() {
+    var e = celularInput.value;
+    e = (e = (e = (e = e.replace(/\D/g, "")).substring(0, 11)).replace(/(\d{2})(\d)/, "($1) $2")).replace(/(\d{1})(\d{4})(\d{4})$/, "$1 $2-$3"), celularInput.value = e
+}));
