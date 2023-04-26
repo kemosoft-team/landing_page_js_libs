628d32a86558882bc13d74a81cc43bbfc8394722
Vitor Almeida <vitorarj@ufrn.edu.br>
2023-04-26 10:47:54 -0300

    feat: criatives

---
+++ creatives.js
@@ -0,0 +1,17 @@
+async function getContactBrandInfo(){
+
+    var url_params = window.location.href;
+    var url = new URL(url_params);
+    let offer = url.searchParams.get("bid"); 
+
+    axios.post('https://api2.kemosoft.com.br/api:workflow/addCreatives', {
+        offerId: "25"
+    })
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    }); 
+  
+  }
