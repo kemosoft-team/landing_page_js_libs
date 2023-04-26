069ac389357f71fd1c6b82c59953dee1b822824f
Vitor Almeida <vitorarj@ufrn.edu.br>
2023-04-26 11:14:35 -0300

    fix: brandId

--- creatives.js
+++ creatives.js
@@ -1,17 +1,16 @@
-async function getContactBrandInfo(){
 
-    var url_params = window.location.href;
-    var url = new URL(url_params);
-    let offer = url.searchParams.get("bid"); 
 
-    axios.get('https://api2.kemosoft.com.br/api:workflow/creatives', {
-        offerId: "25"
-    })
-    .then(function (response) {
-        console.log(response);
-    })
-    .catch(function (error) {
-        console.log(error);
-    }); 
-  
-  }
+var url_params = window.location.href;
+var url = new URL(url_params);
+let offer = url.searchParams.get("id"); 
+
+axios.post('https://api2.kemosoft.com.br/api:workflow/creatives', {
+    offerId: offer
+})
+.then(function (response) {
+    console.log(response);
+})
+.catch(function (error) {
+    console.log(error);
+}); 
+
