e8926606a6017aabaf5c274073a41581410793ef
Vitor Almeida <vitorarj@ufrn.edu.br>
2023-05-08 08:57:58 -0300

    feat: assets update

--- bannerBank.js
+++ bannerBank.js
@@ -1,13 +1,8 @@
 
-
-
-
-
-
 const content = '<div class="brz-css-phzyg brz-wrapper">'+
                     '<div class="brz-image brz-css-pqspb">'+
                         '<picture class="brz-picture brz-d-block brz-p-relative brz-css-hojfk" data-custom-id="potfobpdyrzexyqsnaooisokvmsnehokutoe">'+
-                            '<img class="brz-img" src="assets/bmg-correspondente.png" alt="" draggable="false" loading="lazy"/></picture></div>'+
+                            '<img class="brz-img" src="https://lp-js-libs.s3.sa-east-1.amazonaws.com/assets/bmg_correspondente.png" alt="" draggable="false" loading="lazy"/></picture></div>'+
                             '<div><p class="brz-css-vuboy" data-uniq-id="vwofe" data-generated-css="brz-css-ubaxg">ESTE SITE É 100% SEGURO</p></div';
 
 
