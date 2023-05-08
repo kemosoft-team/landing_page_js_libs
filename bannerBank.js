f4eb9b41a2a248619cf7a944b8b53a58c619e25f
Vitor Almeida <vitorarj@ufrn.edu.br>
2023-05-08 11:30:51 -0300

    style: bank

--- bannerBank.js
+++ bannerBank.js
@@ -15,7 +15,7 @@ const contentHeader =
                     '<div class="brz-image brz-css-pqspb>'+
                         '<picture class="brz-picture brz-d-block brz-p-relative brz-css-hojfk" data-custom-id="potfobpdyrzexyqsnaooisokvmsnehokutoe">'+
                             '<img class="brz-img" style="width:45%" src="https://lp-js-libs.s3.sa-east-1.amazonaws.com/assets/bmg_correspondente.png" alt="" draggable="false" loading="lazy"/></picture></div>'+
-        '<div style="margin-bottom: 5px !important;"><span class="brz-icon brz-span brz-css-ifcxp brz-css-hxdfk inline-block">'+
+        '<div style="min-width: fit-content; margin: auto;"><span class="brz-icon brz-span brz-css-ifcxp brz-css-hxdfk inline-block">'+
         '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 24 24" xml:space="preserve" class="brz-icon-svg align-[initial]" data-type="glyph" data-name="lock-circle">'+
             '<g class="nc-icon-wrapper" fill="currentColor">'+
             '<path fill="currentColor" d="M18,10.726V6c0-3.309-2.691-6-6-6S6,2.691,6,6v4.726C4.759,12.136,4,13.979,4,16c0,4.411,3.589,8,8,8 s8-3.589,8-8C20,13.979,19.241,12.136,18,10.726z M13,17.816L13,20h-2l0-2.184C9.839,17.402,9,16.302,9,15c0-1.654,1.346-3,3-3 s3,1.346,3,3C15,16.302,14.162,17.402,13,17.816z M16,9.082C14.822,8.398,13.458,8,12,8S9.178,8.398,8,9.082V6c0-2.206,1.794-4,4-4 s4,1.794,4,4V9.082z"></path></g></svg></span>'+
