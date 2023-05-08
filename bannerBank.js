3fa49c35afbec30f2eaaa781984457107ee1b101
Vitor Almeida <vitorarj@ufrn.edu.br>
2023-05-08 09:04:28 -0300

    style: p

--- bannerBank.js
+++ bannerBank.js
@@ -3,7 +3,12 @@ const content = '<div class="brz-css-phzyg brz-wrapper">'+
                     '<div class="brz-image brz-css-pqspb">'+
                         '<picture class="brz-picture brz-d-block brz-p-relative brz-css-hojfk" data-custom-id="potfobpdyrzexyqsnaooisokvmsnehokutoe">'+
                             '<img class="brz-img" style="width:45%" src="https://lp-js-libs.s3.sa-east-1.amazonaws.com/assets/bmg_correspondente.png" alt="" draggable="false" loading="lazy"/></picture></div>'+
-                            '<div><p class="brz-css-vuboy" data-uniq-id="vwofe" data-generated-css="brz-css-ubaxg">ESTE SITE É 100% SEGURO</p></div>';
+                            '<div><p class="brz-css-vuboy" style ="margin-top: 0px !important; margin-bottom: 0px !important; text-align: center; font-family: Overpass, sans-serif !important; font-size: 12px; line-height: 1.9; font-weight: 400; letter-spacing: 0px;" data-uniq-id="vwofe" data-generated-css="brz-css-ubaxg">ESTE SITE É 100% SEGURO</p></div>';
 
 
 if(document.getElementById("banner-left")){ document.getElementById("banner-left").innerHTML = content;}
+
+
+
+
+style ="margin-top: 0px !important; margin-bottom: 0px !important; text-align: center; font-family: Overpass, sans-serif !important; font-size: 12px; line-height: 1.9; font-weight: 400; letter-spacing: 0px;"
