105b64f827dde0a6491c7a2aac42c0eac1bdd723
Vitor Almeida <vitorarj@ufrn.edu.br>
2023-05-09 11:27:02 -0300

    refactor

--- fgts-utils.js
+++ fgts-utils.js
@@ -2,8 +2,8 @@ let apiBaseUrl = 'https://api.consigmais.com.br/lp/main/v2/';
 let stepsUrl = 'https://infos.faz.vc/';
 
 function redirectToNextStep(n){
-    console.log(n);
-    // window.location.href = stepsUrl+n
+    // console.log(n);
+    window.location.href = stepsUrl+n
 }
 
 //setar token
