03e25c2ed6b6bf8417842ab71ad35a7cd261a856
Vitor Almeida <vitorarj@ufrn.edu.br>
2023-05-09 09:48:45 -0300

    feat: fgts utils

---
+++ cookies-utils.js
@@ -0,0 +1,55 @@
+//setCookies client origin
+function setCookies(latDays){
+
+  axios.get('https://ipinfo.io/json')
+  .then(function (response) {
+            
+      const ip = response.data.ip;
+      const hostname = response.data.hostname;
+      const city = response.data.city;
+      const region = response.data.region;
+      const country = response.data.country;
+      const loc = response.data.loc;
+      const org = response.data.org;
+
+      let ipinfo = {
+        ip: ip || null,
+        hostname: hostname || null,
+        city: city || null,
+        region: region || null,
+        country: country || null,
+        loc: loc || null,
+        org: org || null,
+    };
+  
+      const urlParams = new URLSearchParams(window.location.search);
+  
+        for (const [key, value] of urlParams.entries()) {
+            ipinfo[key] = value;
+        }
+  
+      var expirationDays = latDays || 7;
+      var expirationDate = new Date();
+      
+      expirationDate.setTime(expirationDate.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
+
+      document.cookie = "client_origin="+encodeURIComponent(JSON.stringify(ipinfo))+"; expires=" + expirationDate.toUTCString() + "; path=/;";
+  })
+  .catch(function (error) {
+      console.log(error);
+  }); 
+}
+
+// obtem o cookie pelo nome 
+function getCookie(name) {
+
+    let cookie = {};
+  
+    document.cookie.split(';').forEach(function(el) {
+      let [k,v] = el.split('=');
+      cookie[k.trim()] = v;
+    })
+  
+    return cookie[name];
+  }
+
---
+++ fgts-utils.js
@@ -0,0 +1,157 @@
+let apiBaseUrl = 'https://api.consigmais.com.br/lp/main/v2/';
+let stepsUrl = 'https://infos.faz.vc/';
+
+function redirectToNextStep(n){
+    // console.log("redirectTon");
+    window.location.href = stepsUrl+n
+}
+
+//setar token
+function handleSetToken(value){
+    // console.log("handleToken");
+    document.cookie = `tkn=${value}; expires=Fri, 31 Dec 9999 23:59:59 GMT; domain=.faz.vc; path=/;`;
+  }
+
+//obtem o step atual pela url
+function getCurrentStep(){
+    const path = window.location.pathname;
+    const value = path.split('/')[1];
+    return value;
+}
+
+//showToast
+function showToast(text) {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    document.getElementById("snackbar").innerHTML = text;
+    setTimeout(function(){ x.className = x.className.replace("show", `${text}`); }, 3000);
+  }
+  
+  //get Token Status
+  function getTokenStatus(){
+  
+    axios.post(APIbaseURL+'/lead-status', {}, {
+      headers: {
+        'Authorization': `Bearer ${getCookie('tkn')}`
+      }})
+      .then(function (response) {
+         response;
+      })
+      .catch(function (error) {
+          console.log(error);
+      }); 
+  }
+
+function setBanks(bankList){
+    bankList.reverse();
+    const selects = document.querySelectorAll('select[data-label="Banco"]');
+    
+    selects.forEach(select => {
+      bankList.forEach(bank => {
+        const option = document.createElement('option');
+        option.text = bank.name;
+        option.value = bank.bankNo;
+        select.insertBefore(option, select.firstChild);
+      });
+    });
+}
+
+//obtem os bancos
+async function getBanks(){
+    axios.post('https://api.consigmais.com.br/lp/main/v2/getData', {"object":"banks"}, {
+      headers: {
+        'Authorization': `Bearer ${getCookie('tkn')}`
+      }})
+      .then(function (response) {
+         setBanks(response.data);
+      })
+      .catch(function (error) {
+          console.log(error);
+      }); 
+  }
+
+
+//obtem os parametros do afiliado oriondos dos cookies
+function captureAffiliateData(){
+
+  if (document.cookie) {
+
+      let affiliateData = {
+          affiliateCode: getCookie('af') || null,
+          source: getCookie('source') || null,
+          productId: getCookie('pid') || null,
+          vendorId: getCookie('vid') || null,
+          offerId: getCookie('oid') || null,
+          clickId : getCookie('cid') || null,
+          pixelId: getCookie('afx') || null,
+          gtmId: getCookie('afgtm') || null,
+          latDays: getCookie('latd') || null,
+          brandId : getCookie('bid') || null,
+          nextStep : getCookie('nxstp') || null,
+          token: getCookie('tkn') || null,
+          rawUri: window.location.search
+      };
+  
+      return affiliateData;
+  }
+}
+
+
+//registerCustomer
+async function registerCustomer(name, birth, federalId, phone, email){
+
+    const button = document.querySelector('.brz-btn-submit');
+    const spinner = button.querySelector('.brz-form-spinner');
+    const span = document.querySelector('.brz-span.brz-text__editor');
+
+    button.setAttribute('disabled', true);
+    spinner.classList.remove('brz-invisible');
+    span.textContent = '';
+  
+    axios.post(APIbaseURL+'/registerCustomer', {
+      "name": name,
+      "birth": birth,
+      "federalId": federalId,
+      "phone": phone,
+      "email": email,
+      "useTerms":true,
+      "dataPrivacy":true,
+      "dataSearchAllowed":true,
+      "affiliateData":{
+		"offerId":"6"
+	  }
+    })
+    .then((response) => {
+      handleSetToken(response.data.token);
+      redirectToNextStep(response.data.nextStep);
+    })
+    .catch(function (error) {
+        button.removeAttribute('disabled');
+        spinner.classList.add('brz-invisible');
+        span.textContent = 'ACEITAR E CONTINUAR';
+        showToast(error.response.data.message);
+    }); 
+}
+
+//validar form
+function validarForm(){ 
+
+    const name = document.querySelector('[data-label="Nome"]').value;
+    const phone = document.querySelector('[data-label="Whatsapp"]').value;
+    const birth = document.querySelector('[data-label="Data de Nascimento"]').value;
+    const email = document.querySelector('[data-label="Email"]').value;
+    const federalId = document.querySelector('[data-label="CPF"]').value;
+  
+    if (name == "" || phone == "" || birth == "" || federalId =="") {
+      showToast("Por favor, preencha todos os campos.");
+      return false;
+    }
+  
+    registerCustomer(name, birth, federalId, phone, email);
+  }
+
+  
+
+
+
+
