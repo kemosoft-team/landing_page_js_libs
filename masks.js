eb303621d485391c04c63dffd4c70775ad2c4a80
Vitor Almeida <vitorarj@ufrn.edu.br>
2023-05-11 09:43:00 -0300

    refactor: condition masks

--- masks.js
+++ masks.js
@@ -1,1 +1,19 @@
-var federalId=document.querySelector('[data-label="CPF"]'),phone=document.querySelector('[data-label="Whatsapp"]'),birth=document.querySelector('[data-label="Data de Nascimento"]');federalId.setAttribute("inputmode","numeric"),phone.setAttribute("inputmode","numeric"),federalId.addEventListener("input",(function(){var e=federalId.value;e=(e=(e=(e=(e=e.replace(/\D/g,"")).substring(0,11)).replace(/(\d{3})(\d)/,"$1.$2")).replace(/(\d{3})(\d)/,"$1.$2")).replace(/(\d{3})(\d{1,2})$/,"$1-$2"),federalId.value=e})),phone.addEventListener("input",(function(){var e=phone.value;e=(e=(e=(e=e.replace(/\D/g,"")).substring(0,11)).replace(/(\d{2})(\d)/,"($1) $2")).replace(/(\d{1})(\d{4})(\d{4})$/,"$1 $2-$3"),phone.value=e})),birth.addEventListener("input",(function(){var e=birth.value;e=(e=(e=(e=e.replace(/\D/g,"")).substring(0,8)).replace(/(\d{2})(\d)/,"$1/$2")).replace(/(\d{2})(\d)/,"$1/$2"),birth.value=e}));
+var federalId = document.querySelector('[data-label="CPF"]'),
+    phone = document.querySelector('[data-label="Whatsapp"]'),
+    birth = document.querySelector('[data-label="Data de Nascimento"]');
+if(federalId){
+federalId.setAttribute("inputmode", "numeric"), phone.setAttribute("inputmode", "numeric"), federalId.addEventListener("input", (function() {
+    var e = federalId.value;
+    e = (e = (e = (e = (e = e.replace(/\D/g, "")).substring(0, 11)).replace(/(\d{3})(\d)/, "$1.$2")).replace(/(\d{3})(\d)/, "$1.$2")).replace(/(\d{3})(\d{1,2})$/, "$1-$2"), federalId.value = e
+}));
+}
+if(phone){phone.addEventListener("input", (function() {
+    var e = phone.value;
+    e = (e = (e = (e = e.replace(/\D/g, "")).substring(0, 11)).replace(/(\d{2})(\d)/, "($1) $2")).replace(/(\d{1})(\d{4})(\d{4})$/, "$1 $2-$3"), phone.value = e
+}));
+}
+if(birth){birth.addEventListener("input", (function() {
+    var e = birth.value;
+    e = (e = (e = (e = e.replace(/\D/g, "")).substring(0, 8)).replace(/(\d{2})(\d)/, "$1/$2")).replace(/(\d{2})(\d)/, "$1/$2"), birth.value = e
+}));
+}
