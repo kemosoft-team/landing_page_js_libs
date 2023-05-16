b1023320cf5a6ded316ada16237848e89e469828
João Carlos <56844688+joaozeira@users.noreply.github.com>
2023-05-16 16:53:00 -0300

    Add files via upload

---
+++ changeContent.js
@@ -0,0 +1,40 @@
+function changeContent() {
+
+    var url = window.location.href;
+    var urlObj = new URL(url);
+    let oid = urlObj.searchParams.get("oid"); 
+
+    switch (oid) {
+        case '18':
+            var btn = document.querySelector('.brz-btn');
+            btn.style.backgroundColor = '#fa6301';
+            btn.href ='http://bmg.faz.vc/';
+
+            var logo = document.querySelector('.brz-img');
+            logo.src = 'https://lp-js-libs.s3.sa-east-1.amazonaws.com/assets/bmg_correspondente.png';
+            break;
+
+        case '11':
+            var btn = document.querySelector('.brz-btn');
+            btn.style.backgroundColor = '#00153a';
+            btn.href = 'http://google.com.br/';
+
+            var logo = document.querySelector('.brz-img');
+            logo.src = 'https://lp-js-libs.s3.sa-east-1.amazonaws.com/assets/master_correspondente.png';
+            break;
+
+        default:
+            // Não faça nada
+            break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
