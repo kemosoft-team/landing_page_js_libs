9903f90f512482b42a5155275a9bfd0e7ff9ecf9
João Carlos <56844688+joaozeira@users.noreply.github.com>
2023-05-25 15:35:58 -0300

    Update leads-conexao.js

--- leads-conexao.js
+++ leads-conexao.js
@@ -82,7 +82,7 @@ async function registerCustomer(name, federalId, phone, email) {
     spinner.classList.remove('brz-invisible');
     span.textContent = '';
 
-    axios.post('https://api.sheetmonkey.io/form/keboAXgkeWL77ZR39TKRLb', {
+    axios.post('https://api.consigmais.com.br/signup/affiliate/register', {
         "name": name,
         "federalId": federalId,
         "mainEmail": email,
