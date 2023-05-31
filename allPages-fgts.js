d9e408763a25ba37433a26964efa7229310e8076
João Carlos <56844688+joaozeira@users.noreply.github.com>
2023-05-31 10:32:42 -0300

    Update allPages-fgts.js

--- allPages-fgts.js
+++ allPages-fgts.js
@@ -259,7 +259,28 @@ getTokenStatus();
 /* ========== */
 var url_params = window.location.href, url = new URL(url_params); let data = url.searchParams.get("bid"); axios.post("https://api.consigmais.com.br/server/lp/main/v2/getBrandInfo", { brandId: data }).then((function (e) { const a = null != e.data.brandLogoDark && window.isWhiteMode ? e.data.brandLogoDark : e.data.brandLogo, o = e.data, t = '<p class="footer" style="text-align: center;">Este produto está sendo oferecido pela</p><br><div style="display: flex;justify-content: center; align-items: center;"><img style="width:100%; max-width:200px;" src="' + a + '" alt="Logomarca ' + o.brandName + '"></div><br><p class="footer terms" style="text-align: center;">Todos os direitos reservados. Todo conteúdo do site, logotipos,marcas, layout, aqui veiculados são de propriedade exclusiva. É vedada qualquer reprodução,total ou parcial, de qualquer elemento de identidade, sem expressa autorização. A violação de qualquer direito mencionado implicará na responsabilização civil e criminal nos termos da Lei.</p><p class="footer links" style="text-align: center;"><a style="color:#fff" href="https://api.consigmais.com.br/terms/">Termos de Uso</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a style="color:#fff" href="https://api.consigmais.com.br/privacy/">Politicas de Privacidade</a></p><p class="footer address" style="text-align: center;">' + o.address + '</p><p class="footer federalid" style="text-align: center;">' + o.federalId + "</p>"; document.getElementById("footer").innerHTML = t })).catch((function (e) { console.log(e) }));
 
+/* ========== TIRAR FUNCIONALIDADE DO BOTÃO ========== */
+<script>var form = document.querySelector('.brz-form');
+form.removeAttribute('action');</script>
 
+/* ========== FORMULARIO INFO POLITICAS ========== */
+<script>
+  //informações adicionais do formulario
+    let novaDiv = document.createElement('div');
+    novaDiv.classList.add('brz-rich-text');
+    let novoParagrafo = document.createElement('p');
+    novoParagrafo.classList.add('brz-css-bmkpa');
+    novoParagrafo.setAttribute('data-generated-css', 'brz-css-gtmtp');
+    novoParagrafo.style.fontSize = '11px';
+    novoParagrafo.style.textAlign = 'justify';
+    novoParagrafo.style.wordWrap = 'break-word';
+    novoParagrafo.innerHTML = '<span class="terms" style="opacity: 1;">Ao continuar no botão abaixo você estará assegurando o tratamento responsável de suas informações, em conformidade com a LGPD vigente, e também: 1. Concordando com os <a href="https://api.consigmais.com.br/terms/" style="color: #646464" target="_blank">Termos de Uso</a> e de <a style="color: #646464" href="https://api.consigmais.com.br/privacy/" target="_blank">Privacidade</a> 2. Aceitando ser contatado por Whatsapp/SMS acerca desta minha consulta e solicitação, bem como autorizo ter meu CPF consultado junto às instituições bancárias e governamentais para assegurar a correta simulação / contratação deste produto.</span>';
+    novaDiv.appendChild(novoParagrafo);
+
+    let divPai = document.querySelector('.brz-forms2__item-button');
+
+    divPai.insertAdjacentElement('beforebegin', novaDiv);
+</script>
 
 
 
