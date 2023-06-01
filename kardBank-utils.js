969a174079039682a64e1968b5c27682e12eaba3
Vitor Almeida <vitorarj@ufrn.edu.br>
2023-06-01 17:48:24 -0300

    feat: kardBank

--- kardBank-utils.js
+++ kardBank-utils.js
@@ -205,3 +205,83 @@ async function registerCustomerAccount(agency, bank, account, verifyDigit, accou
     registerCustomerAccount(agency, bank, account, verifyDigit, accountTypeCut);
   }
 
+
+  
+//obtem os parametros do afiliado oriondos dos cookies
+function captureAffiliateData(){
+
+  const urlParams = new URLSearchParams(window.location.search);
+
+  let affiliateData = {
+
+    affiliateCode: urlParams.get('af') || null,
+    source: urlParams.get('source') || null,
+    productId: urlParams.get('pid') || null,
+    vendorId: urlParams.get('vid') || null,
+    offerId: urlParams.get('oid') || null,
+    clickId : urlParams.get('cid') || null,
+    pixelId: urlParams.get('afx') || null,
+    gtmId: urlParams.get('afgtm') || null,
+    latDays: urlParams.get('latd') || null,
+    brandId : urlParams.get('bid') || null,
+    nextStep : urlParams.get('nxstp') || null,
+    token: urlParams.get('tkn') || null,
+    rawUri: window.location.search
+};
+  return affiliateData;
+}
+
+
+//registerCustomer
+async function registerCustomer(name, birth, federalId, phone, email){
+
+const affiliate = captureAffiliateData();
+
+const button = document.querySelector('.btn-submit-fgts');
+const spinner = button.querySelector('.brz-form-spinner');
+const span = button.querySelector('.brz-span.brz-text__editor');
+
+button.setAttribute('disabled', true);
+spinner.classList.remove('brz-invisible');
+span.textContent = '';
+
+axios.post(apiBaseUrl+'/registerCustomer', {
+  "name": name,
+  "birth": birth,
+  "federalId": federalId,
+  "phone": phone,
+  "email": email,
+  "useTerms":true,
+  "dataPrivacy":true,
+  "dataSearchAllowed":true,
+  "affiliateData": affiliate
+})
+.then((response) => {
+  handleSetToken(response.data.token);
+  redirectToNextStep(response.data.nextStep);
+})
+.catch(function (error) {
+    button.removeAttribute('disabled');
+    spinner.classList.add('brz-invisible');
+    span.textContent = 'ACEITAR E CONTINUAR';
+    showToast(error.response.data.message);
+}); 
+}
+
+//validar form
+function validateForm(){ 
+
+const name = document.querySelector('[data-label="Nome"]').value;
+const phone = document.querySelector('[data-label="Whatsapp"]').value;
+const birth = document.querySelector('[data-label="Data de Nascimento"]').value;
+const email = document.querySelector('[data-label="Email (Opcional)"]').value;
+const federalId = document.querySelector('[data-label="CPF"]').value;
+
+if (name == "" || phone == "" || birth == "" || federalId =="") {
+  showToast("Por favor, preencha todos os campos.");
+  return false;
+}
+
+registerCustomer(name, birth, federalId, phone, email);
+}
+
