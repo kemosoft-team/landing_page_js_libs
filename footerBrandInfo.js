70eee424924ff46658d3d10f650e3781848e1645
Vitor Almeida <vitorarj@ufrn.edu.br>
2023-06-05 10:44:12 -0300

    refactor: footer disclaimer

--- footerBrandInfo.js
+++ footerBrandInfo.js
@@ -1,21 +1,56 @@
-            var url_params = window.location.href;
-                var url = new URL(url_params);
-                let data = url.searchParams.get("bid");
+var url_params = window.location.href;
+var url = new URL(url_params);
+let bid = url.searchParams.get("bid");
+let oid = url.searchParams.get("oid");
 
-                axios.post("https://api.consigmais.com.br/server/lp/main/v2/getBrandInfo", { brandId: data })
-                        .then(function (e) {
-                                const a = null != e.data.brandLogoDark && window.isWhiteMode ? e.data.brandLogoDark : e.data.brandLogo;
-                                const o = e.data;
-                                const t = '<p class="footer" style="text-align: center;">Este produto está sendo oferecido pela intermediadora financeira:</p><br>' +
-                                        '<div style="display: flex;justify-content: center; align-items: center;"><img style="width:100%; max-width:200px;" src="' + a + '" alt="Logomarca ' + o.brandName + '"></div><br>' +
-                                        '<p class="footer terms" style="text-align: center;">Todos os direitos reservados. Todo conteúdo do site, logotipos, marcas, layout, aqui veiculados são de propriedade exclusiva. É vedada qualquer reprodução, total ou parcial, de qualquer elemento de identidade, sem expressa autorização. A violação de qualquer direito mencionado implicará na responsabilização civil e criminal nos termos da Lei.</p>' +
-                                        '<p class="footer links" style="text-align: center;"><a style="color:#fff" href="https://api.consigmais.com.br/terms/">Termos de Uso</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a style="color:#fff" href="https://api.consigmais.com.br/privacy/">Politicas de Privacidade</a></p>' +
-                                        '<p class="footer address" style="text-align: center;">' + o.address + '</p>' +
-                                        '<p class="footer federalid" style="text-align: center;">CNPJ:  ' + o.federalId + '</p>';
+axios.post("https://api.consigmais.com.br/server/lp/main/v2/getBrandInfo", { brandId: bid })
+        .then(function (e) {
+                
+                const t  = '';
+                const a = null != e.data.brandLogoDark && window.isWhiteMode ? e.data.brandLogoDark : e.data.brandLogo;
+                const o = e.data;
 
-                                document.getElementById("footer").innerHTML = t;
-                        })
-                        .catch(function (e) {
-                                console.log(e);
-                        });
+                switch (oid) {
+                        //BMG
+                        case '18':
+                          t = '<footer style="display: flex; justify-content: space-between; padding: 10px;">'+
+                                '<div class="logo">'+
+                                '<img src="'+ a +'" alt="Logo" style="width: 100px;"></div>'+
+                                '<div class="terms">'+
+                                '<p style="text-align: justify; width: 70%;">1. Sujeito à análise de crédito e incidência de encargos. 2. As parcelas antecipadas serão pagas anualmente,'
+                                'no mês em que seria efetuado o saque aniversário, mediante repasse do valor pela CEF ao banco BMG. 3. Necessário'
+                                'aderir à modalidade Saque Aniversário do FGTS e autorizar o banco BMG no APP oficial do FGTS ou pelo site da CEF.'
+                                '4 Produto oferecido pela Consigmais, intermediador financeiro Banco BMG, '+ o.address +', CNPJ '+ o.federalId +'.</p>'+
+                                '</div>'+
+                                '</footer>';
+                          break;
+                        //Banco Master
+                        case '29':
+                          t = 'master_correspondente.png';
+                          break;
+                        //Default
+                        default:
+                          t = '<p class="footer" style="text-align: center;">Este produto está sendo oferecido pela intermediadora financeira:</p><br>' +
+                          '<div style="display: flex;justify-content: center; align-items: center;"><img style="width:100%; max-width:200px;" src="' + a + '" alt="Logomarca ' + o.brandName + '"></div><br>' +
+                          '<p class="footer terms" style="text-align: center;">Todos os direitos reservados. Todo conteúdo do site, logotipos, marcas, layout, aqui veiculados são de propriedade exclusiva. É vedada qualquer reprodução, total ou parcial, de qualquer elemento de identidade, sem expressa autorização. A violação de qualquer direito mencionado implicará na responsabilização civil e criminal nos termos da Lei.</p>' +
+                          '<p class="footer links" style="text-align: center;"><a style="color:#fff" href="https://api.consigmais.com.br/terms/">Termos de Uso</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a style="color:#fff" href="https://api.consigmais.com.br/privacy/">Politicas de Privacidade</a></p>' +
+                          '<p class="footer address" style="text-align: center;">' + o.address + '</p>' +
+                          '<p class="footer federalid" style="text-align: center;">CNPJ:  ' + o.federalId + '</p>';
+                          break;
+                      }
+
+
+                
+                // t = '<p class="footer" style="text-align: center;">Este produto está sendo oferecido pela intermediadora financeira:</p><br>' +
+                //         '<div style="display: flex;justify-content: center; align-items: center;"><img style="width:100%; max-width:200px;" src="' + a + '" alt="Logomarca ' + o.brandName + '"></div><br>' +
+                //         '<p class="footer terms" style="text-align: center;">Todos os direitos reservados. Todo conteúdo do site, logotipos, marcas, layout, aqui veiculados são de propriedade exclusiva. É vedada qualquer reprodução, total ou parcial, de qualquer elemento de identidade, sem expressa autorização. A violação de qualquer direito mencionado implicará na responsabilização civil e criminal nos termos da Lei.</p>' +
+                //         '<p class="footer links" style="text-align: center;"><a style="color:#fff" href="https://api.consigmais.com.br/terms/">Termos de Uso</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a style="color:#fff" href="https://api.consigmais.com.br/privacy/">Politicas de Privacidade</a></p>' +
+                //         '<p class="footer address" style="text-align: center;">' + o.address + '</p>' +
+                //         '<p class="footer federalid" style="text-align: center;">CNPJ:  ' + o.federalId + '</p>';
+
+                document.getElementById("footer").innerHTML = t;
+        })
+        .catch(function (e) {
+                console.log(e);
+        });
 
