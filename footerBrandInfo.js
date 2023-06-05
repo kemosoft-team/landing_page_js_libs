a0ef07b75062f643c43ee802d78794384758ebfa
Vitor Almeida <vitorarj@ufrn.edu.br>
2023-06-05 11:06:20 -0300

    refactor: footer disclaimer

--- footerBrandInfo.js
+++ footerBrandInfo.js
@@ -39,15 +39,6 @@ axios.post("https://api.consigmais.com.br/server/lp/main/v2/getBrandInfo", { bra
                           break;
                       }
 
-
-                
-                // t = '<p class="footer" style="text-align: center;">Este produto está sendo oferecido pela intermediadora financeira:</p><br>' +
-                //         '<div style="display: flex;justify-content: center; align-items: center;"><img style="width:100%; max-width:200px;" src="' + a + '" alt="Logomarca ' + o.brandName + '"></div><br>' +
-                //         '<p class="footer terms" style="text-align: center;">Todos os direitos reservados. Todo conteúdo do site, logotipos, marcas, layout, aqui veiculados são de propriedade exclusiva. É vedada qualquer reprodução, total ou parcial, de qualquer elemento de identidade, sem expressa autorização. A violação de qualquer direito mencionado implicará na responsabilização civil e criminal nos termos da Lei.</p>' +
-                //         '<p class="footer links" style="text-align: center;"><a style="color:#fff" href="https://api.consigmais.com.br/terms/">Termos de Uso</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a style="color:#fff" href="https://api.consigmais.com.br/privacy/">Politicas de Privacidade</a></p>' +
-                //         '<p class="footer address" style="text-align: center;">' + o.address + '</p>' +
-                //         '<p class="footer federalid" style="text-align: center;">CNPJ:  ' + o.federalId + '</p>';
-
                 document.getElementById("footer").innerHTML = t;
         })
         .catch(function (e) {
