edae164d131195b1b851d5db2f94718536ce442c
Vitor Almeida <vitorarj@ufrn.edu.br>
2023-06-05 17:21:39 -0300

    birth input numeric mode

--- masks.js
+++ masks.js
@@ -2,7 +2,7 @@ var federalId = document.querySelector('[data-label="CPF"]'),
     phone = document.querySelector('[data-label="Whatsapp"]'),
     birth = document.querySelector('[data-label="Data de Nascimento"]');
 if(federalId){
-federalId.setAttribute("inputmode", "numeric"), phone.setAttribute("inputmode", "numeric"), federalId.addEventListener("input", (function() {
+federalId.setAttribute("inputmode", "numeric"), phone.setAttribute("inputmode", "numeric"), birth.setAttribute("inputmode", "numeric"), federalId.addEventListener("input", (function() {
     var e = federalId.value;
     e = (e = (e = (e = (e = e.replace(/\D/g, "")).substring(0, 11)).replace(/(\d{3})(\d)/, "$1.$2")).replace(/(\d{3})(\d)/, "$1.$2")).replace(/(\d{3})(\d{1,2})$/, "$1-$2"), federalId.value = e
 }));
