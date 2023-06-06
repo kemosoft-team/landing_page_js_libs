dc5de64bbcd6f7b0d24ede4aafdc31e9ce0719de
João Carlos <56844688+joaozeira@users.noreply.github.com>
2023-06-06 11:11:20 -0300

    Update secondyForm.js

--- secondyForm.js
+++ secondyForm.js
@@ -27,7 +27,7 @@ async function secondyRegisterCustomer(name, phone, email) {
         site: currentUrl
     })
         .then((response) => {
-            window.location.href = "https://blackcurrant16889289.kemobuilder.site/";
+            window.location.href = currentUrl;
         })
         .catch(function (error) {
             buttonSecondy.removeAttribute('disabled');
