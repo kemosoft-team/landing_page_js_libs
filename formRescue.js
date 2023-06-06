8c3a8646554c0030e4f4586a1de58db262404646
João Carlos <56844688+joaozeira@users.noreply.github.com>
2023-06-06 15:20:20 -0300

    Update formRescue.js

--- formRescue.js
+++ formRescue.js
@@ -1,10 +1,13 @@
 //======FORMULARIO DE RESGATE========
 //ABRIR FOMULARIO AO TIRAR O MOUSE DA TELA
 // #btnOut é o botão que vai ser clicado ao tirar o mouse da tela
+var statusPopUp = false;
+
 btnOut = document.querySelector('#btnOut');
 document.addEventListener("mouseout", function (event) {
-    if (event.clientY <= 0) {
+    if (event.clientY <= 0 && statusPopUp === false) {
         btnOut.click();
+        statusPopUp = true;
     }
 });
 
@@ -21,17 +24,18 @@ var isScrolling = false;
 
 //SE SCROLL PASSAR DE 75%
 window.addEventListener('scroll', function () {
-    if (!isScrolling && window.scrollY >= scrollThreshold) {
+    if (!isScrolling && window.scrollY >= scrollThreshold && statusPopUp === false) {
         isScrolling = true;
+        statusPopUp = true;
     }
 });
 
 //SE VOLTAR PARA TOP MENOR QUE 100
 window.addEventListener('scroll', function () {
-    if (isScrolling && window.scrollY <= 100) {
+    if (isScrolling && window.scrollY <= 100 && statusPopUp === false) {
         isScrolling = false;
         btnWillOpen.click()
+        statusPopUp = true;
     }
 });
-
 
