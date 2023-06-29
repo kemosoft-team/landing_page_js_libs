aad99ea6d0202e6e2f28ed9ceb5bfac2b9c69451
Vitor Almeida <vitorarj@ufrn.edu.br>
2023-06-29 15:10:52 -0300

    fix: remove caracteres especiais da agencia banco

--- fgts-utils.js
+++ fgts-utils.js
@@ -3,7 +3,6 @@ if (typeof apiBaseUrl == 'undefined') {
   let apiBaseUrl = 'https://api.consigmais.com.br/lp/main/v2/';
   let stepsUrl = 'https://correspondente-bmg.faz.vc/';
 }
-
 //inicia spin loading no button
 function setLoading(){
   button.setAttribute('disabled', true);
