65dc88c3b9a3945bf857301b24776f4b0690f864
Vitor Almeida <vitorarj@ufrn.edu.br>
2023-06-29 12:28:05 -0300

    feat: new-affiliate

--- new-affiliate-utils.js
+++ new-affiliate-utils.js
@@ -38,7 +38,7 @@ async function registerAffiliate(name, birth, federalId, zipcode, phone, email){
     .catch(function (error) {
         button.removeAttribute('disabled');
         spinner.classList.add('brz-invisible');
-        span.textContent = 'Quero me cadastrar';
+        span.textContent = 'Aceita e Cadastrar';
         showToast(error.response.data.message);
     }); 
 }
@@ -70,6 +70,7 @@ function validateForm(){
         novoParagrafo.setAttribute('data-generated-css', 'brz-css-gtmtp');
         novoParagrafo.style.fontSize = '11px';
         novoParagrafo.style.textAlign = 'justify';
+        novoParagrafo.style.padding = '0 15px 0 15px';
         novoParagrafo.style.wordWrap = 'break-word';
         novoParagrafo.innerHTML = '<span class="terms" style="opacity: 1;">Ao continuar no botão abaixo você estará assegurando o tratamento responsável de suas informações, em conformidade com a LGPD vigente, e também: 1. Concordando com os <a href="https://api.consigmais.com.br/terms/" style="color: #646464" target="_blank">Termos de Uso</a> e de <a style="color: #646464" href="https://api.consigmais.com.br/privacy/" target="_blank">Privacidade</a> 2. Aceitando ser contatado por Whatsapp/SMS acerca desta minha consulta e solicitação, bem como autorizo ter meu CPF consultado junto às instituições bancárias e governamentais para assegurar a correta simulação / contratação deste produto.</span>';
         novaDiv.appendChild(novoParagrafo);
