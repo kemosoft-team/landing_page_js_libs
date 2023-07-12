36be8cad0694a4f239d95e3fe2bd26e35ea565ca
João Carlos <56844688+joaozeira@users.noreply.github.com>
2023-07-12 12:51:27 -0300

    Update secondyForm.js

--- secondyForm.js
+++ secondyForm.js
@@ -1,27 +1,28 @@
 var urlApi = ('https://api.sheetmonkey.io/form/toQEKxvQa6TUiyLJ6td4hM');
 
-var cpfInput = document.querySelector('[data-label="Deixe seu CPF"]');
-var whatsappInput = document.querySelector('[data-label="Deixe seu Whatsapp"]');
+var federalId = document.querySelector('[data-label="Deixe seu CPF"]');
+var phone = document.querySelector('[data-label="Deixe seu Whatsapp"]');
 
-if (cpfInput) {
-  cpfInput.addEventListener("input", function () {
-    var cpf = cpfInput.value;
+if (federalId) {
+  federalId.addEventListener("input", function () {
+    var cpf = federalId.value;
     cpf = cpf.replace(/\D/g, ""); // Remove todos os caracteres não numéricos
     cpf = cpf.slice(0, 11); // Limita o valor do CPF a 11 dígitos
     cpf = cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4"); // Aplica a máscara de CPF
-    cpfInput.value = cpf;
+    federalId.value = cpf;
   });
 }
 
-if (whatsappInput) {
-  whatsappInput.addEventListener("input", function () {
-    var phone = whatsappInput.value;
-    phone = phone.replace(/\D/g, ""); // Remove todos os caracteres não numéricos
-    phone = phone.slice(0, 11); // Limita o valor do WhatsApp a 11 dígitos
-    phone = phone.replace(/(\d{2})(\d{1,5})(\d{4})/, "($1) $2-$3"); // Aplica a máscara de WhatsApp
-    whatsappInput.value = phone;
+if (phone) {
+  phone.addEventListener("input", function () {
+    var phoneNumber = phone.value;
+    phoneNumber = phoneNumber.replace(/\D/g, ""); // Remove todos os caracteres não numéricos
+    phoneNumber = phoneNumber.slice(0, 11); // Limita o valor do WhatsApp a 11 dígitos
+    phoneNumber = phoneNumber.replace(/(\d{2})(\d{1,5})(\d{4})/, "($1) $2-$3"); // Aplica a máscara de WhatsApp
+    phone.value = phoneNumber;
   });
 }
+
 
 // secondyRegisterCustomer
 async function secondyRegisterCustomer(name, phone, federalId) {
