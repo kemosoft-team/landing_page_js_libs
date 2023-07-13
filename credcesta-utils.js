c832f2eb23ade7095937f9c3f45f1c68e0877a1d
Vitor Almeida <vitorarj@ufrn.edu.br>
2023-07-13 14:52:48 -0300

    feat: scripts steps

--- credcesta-utils.js
+++ credcesta-utils.js
@@ -2,7 +2,7 @@
 let apiUrl = 'https://api.consigmais.com.br/lp/main/v2/';
 let stepsUrls = 'https://credcesta.kemobuilder.site/';
 
-// obtem o cookie pelo nome 
+//Obtem o cookie pelo nome 
 function getCookie(name) {
 
   let cookie = {};
@@ -15,17 +15,46 @@ function getCookie(name) {
   return cookie[name];
 }
 
-function redirectToNextStep(n) {
-  window.location.replace(`${stepsUrls + n}`);
+//Obtem e redireciona para nextstep atraves de consulta com token
+function setNextStep() {
+
+  axios.post(apiBaseUrl+'getTokenStatus', {}, {
+    headers: {
+      'Authorization': `Bearer ${getCookie('tkn')}`
+    }})
+    .then(function (response) {
+      window.location.href = stepsUrl+response.data.nextStep;
+    })
+    .catch(function (error) {
+        console.log(error);
+    }); 
 }
 
-//setar token
+//Redireciona para subpágina
+function redirectToNextStep(res) {
+
+  const param = window.location.search || '';
+  const nextStep = res.nextStep;
+
+  switch (nextStep) {
+    case 'signature':
+      window.location.href = stepsUrl + nextStep + param + '&' + encodeURIComponent(JSON.stringify(res.formalizatioLink));
+      break;
+    case 'scheduled':
+      window.location.href = stepsUrl + nextStep + param + '&' + encodeURIComponent(JSON.stringify(res.scheduledTo));
+      break;
+    default:
+      window.location.href = stepsUrl + nextStep + param;
+      break;
+  }
+}
+
+//Seta cookie
 function handleSetToken(value) {
-  // console.log("handleToken");
   document.cookie = `tkn=${value}; expires=Fri, 31 Dec 9999 23:59:59 GMT; domain=.kemobuilder.site; path=/;`;
 }
 
-//obtem o step atual pela url
+//Obtem o step atual pela url
 function getCurrentStep() {
   const path = window.location.pathname;
   const value = path.split('/')[1];
@@ -45,7 +74,7 @@ function getTokenStatus() {
       .then(function (response) {
 
         const link = document.querySelector('a.btn-continue');
-        redirectToNextStep(n)
+        link.setAttribute('href', stepsUrl + response.data.nextStep + param);
 
         document.getElementById("info-return").innerHTML = `<p class="p-info-return">${response.data.message}</p>`;
         var botao = document.querySelector(".btn-lead-info");
@@ -58,7 +87,7 @@ function getTokenStatus() {
   }
 }
 
-//showToast
+//Exibe mensagem no toast
 function showToast(text) {
   var x = document.getElementById("snackbar");
   x.className = "show";
@@ -66,7 +95,7 @@ function showToast(text) {
   setTimeout(function () { x.className = x.className.replace("show", `${text}`); }, 3000);
 }
 
-
+//Popula o select de bancos
 function setBanks(bankList) {
   bankList.reverse();
   const selects = document.querySelectorAll('select[data-label="Banco"]');
@@ -81,7 +110,7 @@ function setBanks(bankList) {
   });
 }
 
-//obtem os bancos
+//Obtem os bancos
 async function getBanks() {
   axios.post('https://api.consigmais.com.br/lp/main/v2/getData', { "object": "banks" }, {
     headers: {
@@ -97,7 +126,7 @@ async function getBanks() {
 }
 
 
-//obtem os parametros do afiliado oriondos dos cookies
+//Obtem os parametros do afiliado oriondos dos cookies
 function captureAffiliateData() {
 
   const urlParams = new URLSearchParams(window.location.search);
@@ -157,7 +186,303 @@ async function registerCustomer(name, birth, federalId, phone) {
     });
 }
 
-//validar form
+
+//Obtem as informamações de endereço com base no CEP
+async function getByZipCodeInfo(zipcode){
+  
+  axios.post(apiBaseUrl+'getZipcodeInfo', {
+    zipcode: zipcode,
+  },
+  {
+    headers: {
+      'Authorization': `Bearer ${getCookie('tkn')}`
+    }})
+  .then((response) => {
+    setAddressInfo(response.data);
+  })
+  .catch(function (error) {
+      showToast(error.response.data.message);
+  }); 
+
+}
+
+//Preenche os campos de endereço do form
+function setAddressInfo(obj){
+  document.querySelector('[data-label="Rua"]').value = obj.address;
+  document.querySelector('[data-label="Bairro"]').value = obj.district;
+  document.querySelector('[data-label="Cidade"]').value = obj.city;
+  document.querySelector('[data-label="UF"]').value = obj.state;
+}
+
+  
+  //registerCustomerAccount
+  async function registerCustomerAddress(zipcode, address, addressNumber, state, district, city) {
+  
+    const button = document.querySelector('.brz-btn-submit');
+    const spinner = button.querySelector('.brz-form-spinner');
+    const span = button.querySelector('.brz-span.brz-text__editor');
+  
+    button.setAttribute('disabled', true);
+    spinner.classList.remove('brz-invisible');
+    span.textContent = '';
+    
+    axios.post(apiBaseUrl+'registerCustomerInfos', {
+      zipcode: zipcode,
+      address: address, 
+      addressNumber: addressNumber, 
+      state: state, 
+      district: district, 
+      city: city,
+      currentStep: getCurrentStep()
+    },
+    {
+      headers: {
+        'Authorization': `Bearer ${getCookie('tkn')}`
+      }})
+    .then((response) => {
+      redirectToNextStep(response.data);
+    })
+    .catch(function (error) {
+        button.removeAttribute('disabled');
+        spinner.classList.add('brz-invisible');
+        span.textContent = 'Sim, quero antecipar meu FGTS!';
+        showToast(error.response.data.message);
+    }); 
+  
+    }
+  
+  //registerCustomerAccount
+  async function registerCustomerAccount(agency, bank, account, verifyDigit, accountType) {
+  
+  const button = document.querySelector('.brz-btn-submit');
+  const spinner = button.querySelector('.brz-form-spinner');
+  const span = button.querySelector('.brz-span.brz-text__editor');
+  
+  button.setAttribute('disabled', true);
+  spinner.classList.remove('brz-invisible');
+  span.textContent = '';
+  
+  axios.post(apiBaseUrl+'registerCustomerInfos', {
+    branchNo: agency.replace(/[^\w\s]/gi, ''),
+    bankId: bank,
+    acctNo: `${account}-${verifyDigit}`,
+    acctType: accountType,
+    currentStep: getCurrentStep()
+  },
+  {
+    headers: {
+      'Authorization': `Bearer ${getCookie('tkn')}`
+    }})
+  .then((response) => {
+    redirectToNextStep(response.data);
+  })
+  .catch(function (error) {
+      button.removeAttribute('disabled');
+      spinner.classList.add('brz-invisible');
+      span.textContent = 'Simular';
+      showToast(error.response.data.message);
+  }); 
+  
+  }
+  // registerCustomerAccount
+  async function registerCustomerDocs(docNumber, docType, issueState, motherName) {
+  
+  const button = document.querySelector('.brz-btn-submit');
+  const spinner = button.querySelector('.brz-form-spinner');
+  const span = button.querySelector('.brz-span.brz-text__editor');
+  
+  button.setAttribute('disabled', true);
+  spinner.classList.remove('brz-invisible');
+  span.textContent = '';
+  
+  axios.post(apiBaseUrl+'registerCustomerInfos', {
+    docNumber: docNumber,
+    docType: docType,
+    docState: issueState,
+    mother: motherName,
+    currentStep: getCurrentStep()
+  },
+  {
+    headers: {
+      'Authorization': `${getCookie('tkn')}`
+    }})
+  .then((response) => {
+    redirectToNextStep(response.data);
+  })
+  .catch(function (error) {
+      button.removeAttribute('disabled');
+      spinner.classList.add('brz-invisible');
+      span.textContent = 'Sim, quero antecipar meu FGTS!';
+      showToast(error.response.data.message);
+  }); 
+  
+  }
+
+    // registerCustomerBenefit
+    async function registerCustomerDocs(docNumber, docType, issueState, motherName) {
+  
+      const button = document.querySelector('.brz-btn-submit');
+      const spinner = button.querySelector('.brz-form-spinner');
+      const span = button.querySelector('.brz-span.brz-text__editor');
+      
+      button.setAttribute('disabled', true);
+      spinner.classList.remove('brz-invisible');
+      span.textContent = '';
+      
+      axios.post(apiBaseUrl+'registerCustomerInfos', {
+        docNumber: docNumber,
+        docType: docType,
+        docState: issueState,
+        mother: motherName,
+        currentStep: getCurrentStep()
+      },
+      {
+        headers: {
+          'Authorization': `${getCookie('tkn')}`
+        }})
+      .then((response) => {
+        redirectToNextStep(response.data);
+      })
+      .catch(function (error) {
+          button.removeAttribute('disabled');
+          spinner.classList.add('brz-invisible');
+          span.textContent = 'Sim, quero antecipar meu FGTS!';
+          showToast(error.response.data.message);
+      }); 
+      
+      }
+  
+  function getNextStep(){
+
+    const attempts = localStorage.getItem('attempts') || 0;
+  
+    const button = document.querySelector('.brz-btn-submit');
+    const spinner = button.querySelector('.brz-form-spinner');
+    const span = button.querySelector('.brz-span.brz-text__editor');
+  
+    axios.post(apiBaseUrl+'getNextStep', {}, 
+    {
+      headers: {
+        'Authorization': `${getCookie('tkn')}`
+      }
+    })
+    .then((response) => {
+
+      if((attempts == 2) && (response.data.nextStep == 'keepcalm')){
+          window.location.href = stepsUrl + 'offline';
+      }else{
+
+  
+      if(response.data.nextStep == 'noBalance' || response.data.nextStep == 'authorize' || response.data.nextStep == 'enable'){
+
+          if(response.data.nextStep == 'authorize'){
+            const authorizeLimit = localStorage.getItem('authorizeLimit') || 0;
+            localStorage.setItem('authorizeLimit', parseInt(authorizeLimit) + 1);
+          }
+          window.location.href = stepsUrl+response.data.nextStep;
+  
+      }else{
+  
+        var elementsWait = document.getElementsByClassName('wait');
+        var elementsSuccess = document.getElementsByClassName('success');
+  
+        for (var i = 0; i < elementsWait.length; i++) {
+          elementsWait[i].style.display = 'none';
+          elementsSuccess[i].style.display = 'block';
+        }
+  
+        button.removeAttribute('disabled');
+        spinner.classList.add('brz-invisible');
+        span.textContent = 'Dê o próximo passo, preencha seus dados';
+        
+        button.addEventListener('click', function() {
+              window.location.href = stepsUrl+response.data.nextStep;
+        });
+      }
+
+      }
+  
+      })
+      .catch(function (error) {
+      });
+  
+  }
+
+//validarFormAddress
+function validarFormAddress(){ 
+  
+  const zipcode = document.querySelector('[data-label="CEP"]').value;
+  const address = document.querySelector('[data-label="Rua"]').value;
+  const addressNumber = document.querySelector('[data-label="Número"]').value;
+  const state = document.querySelector('[data-label="UF"]').value;
+  const district = document.querySelector('[data-label="Bairro"]').value;
+  const city = document.querySelector('[data-label="Cidade"]').value;
+
+  
+  if (zipcode == "" || address == "" || addressNumber == "" || state =="" || district =="" || city =="") {
+    showToast("Por favor, preencha todos os campos.");
+    return false;
+  }
+  registerCustomerAddress(zipcode, address, addressNumber, state, district, city);
+  
+}
+
+//validarFormDocs
+function validarFormDocs(){ 
+
+  const docType = document.querySelector('[data-label="Tipo de Documento"]').value;
+  const docNumber = document.querySelector('[data-label="Número do Documento"]').value;
+  const issueState = document.querySelector('[data-label="UF Expeditor"]').value;
+  const motherName = document.querySelector('[data-label="Nome da sua Mãe"]').value;
+
+  
+  if (docType == "" || docNumber == "" || issueState == "" || motherName =="") {
+    showToast("Por favor, preencha todos os campos.");
+    return false;
+  }
+  registerCustomerDocs(docNumber, docType, issueState, motherName);
+}
+
+//validarFormDocs
+function validarFormBenefit(){ 
+
+  const docType = document.querySelector('[data-label="Tipo de Documento"]').value;
+  const docNumber = document.querySelector('[data-label="Número do Documento"]').value;
+  const issueState = document.querySelector('[data-label="UF Expeditor"]').value;
+  const motherName = document.querySelector('[data-label="Nome da sua Mãe"]').value;
+
+  
+  if (docType == "" || docNumber == "" || issueState == "" || motherName =="") {
+    showToast("Por favor, preencha todos os campos.");
+    return false;
+  }
+  registerCustomerDocs(docNumber, docType, issueState, motherName);
+}
+
+function validarFormAccount(){ 
+
+  const agency = document.querySelector('[data-label="Agência"]').value;
+  var bank = '';
+  const account = document.querySelector('[data-label="Conta"]').value;
+  const verifyDigit = document.querySelector('[data-label="Dígito"]').value;
+  const accountType = document.querySelector('[data-label="Tipo de conta"]').value;
+
+    if(document.querySelectorAll('div.brz-forms2__item')[1].style.display == "block"){
+      bank = document.querySelector('[data-label="Nome Banco"]').value;
+    }else{
+      bank = document.querySelector('[data-label="Banco"]').value;
+    }
+
+  if (agency == "" || bank == "" || account == "" || verifyDigit =="" || accountType =="") {
+    showToast("Por favor, preencha todos os campos.");
+    return false;
+  }
+
+  const  accountTypeCut = accountType.charAt(0).toString();
+  registerCustomerAccount(agency, bank, account, verifyDigit, accountTypeCut);
+}
+
+//valida form
 function validateForm() {
 
   const name = document.querySelector('[data-label="Nome"]').value;
@@ -174,3 +499,6 @@ function validateForm() {
 }
 
 getTokenStatus();
+
+
+
