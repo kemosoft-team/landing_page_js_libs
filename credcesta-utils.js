0e0102ad32fb1b826ed00b69d4fa255cf4bbaf5b
João Carlos <56844688+joaozeira@users.noreply.github.com>
2023-07-14 08:56:59 -0300

    Update credcesta-utils.js

--- credcesta-utils.js
+++ credcesta-utils.js
@@ -168,6 +168,7 @@ async function registerCustomer(name, birth, federalId, phone, federalIdRepresen
     "name": name,
     "birth": birth,
     "federalId": federalId,
+    "federalIdRepresent": federalIdRepresent,
     "phone": phone,
     "email":"", 
     "useTerms": true,
