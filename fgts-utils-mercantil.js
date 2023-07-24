feca4d97199587fff5b44a19b86450021c56a230
João Carlos <56844688+joaozeira@users.noreply.github.com>
2023-07-24 10:00:45 -0300

    Update fgts-utils-mercantil.js

--- fgts-utils-mercantil.js
+++ fgts-utils-mercantil.js
@@ -183,14 +183,3 @@ if (window.location.pathname == '/fgts') {
   let divPai = document.querySelector('.brz-forms2__item-button');
   divPai.insertAdjacentElement('beforebegin', novaDiv);
 
-  //Setar videos 
-  var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-
-  if (screenWidth > 767) {
-    const desktopContent = '<div><div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;"><figure style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%; margin-block-end: 0; margin-block-start: 0; margin-inline-start: 0; margin-inline-end: 0;" ><iframe src="https://api.vadoo.tv/iframe_test?id=qlbmxwIzmkuf6UWl1aJVHRMmTmvG7kQy" scrolling="no" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute; overflow:hidden; border-radius: 5px;" allowfullscreen="1" allow="autoplay"></iframe></figure></div></div>';
-    document.getElementById('video-content').innerHTML = desktopContent;
-  } else {
-    const mobileContent = '<div><div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 177.78%;"><figure style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 177.78%; margin-block-end: 0; margin-block-start: 0; margin-inline-start: 0; margin-inline-end: 0;" ><iframe src="https://api.vadoo.tv/iframe_test?id=Isk5HRNYtWRHfmrQi7ptXjHNLlNJA1oL" scrolling="no" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute; overflow:hidden; border-radius: 5px;" allowfullscreen="1" allow="autoplay"></iframe></figure></div></div>';
-    document.getElementById('video-content').innerHTML = mobileContent;
-  }
-}
