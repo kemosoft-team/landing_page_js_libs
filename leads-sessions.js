850aca3b4a241a18a83a31d2cf6d6fa13294e7cc
João Carlos <56844688+joaozeira@users.noreply.github.com>
2023-08-15 12:51:10 -0300

    Update leads-sessions.js

--- leads-sessions.js
+++ leads-sessions.js
@@ -1,120 +1,147 @@
 ///get cookies
 function getCookie(name) {
 
-  let cookie = {};
+    let cookie = {};
 
-  document.cookie.split(';').forEach(function (el) {
-      let [k, v] = el.split('=');
-      cookie[k.trim()] = v;
-  })
+    document.cookie.split(';').forEach(function (el) {
+        let [k, v] = el.split('=');
+        cookie[k.trim()] = v;
+    })
 
-  return cookie[name];
+    return cookie[name];
 }
 
 
 //seta cookies
 function setCookies(latDays) {
 
-  axios.get('https://ipinfo.io/json')
-      .then(function (response) {
+    axios.get('https://ipinfo.io/json')
+        .then(function (response) {
 
-          const ip = response.data.ip;
-          const hostname = response.data.hostname;
-          const city = response.data.city;
-          const region = response.data.region;
-          const country = response.data.country;
-          const loc = response.data.loc;
-          const org = response.data.org;
+            const ip = response.data.ip;
+            const hostname = response.data.hostname;
+            const city = response.data.city;
+            const region = response.data.region;
+            const country = response.data.country;
+            const loc = response.data.loc;
+            const org = response.data.org;
 
-          let ipinfo = {
-              ip: ip || null,
-              hostname: hostname || null,
-              city: city || null,
-              region: region || null,
-              country: country || null,
-              loc: loc || null,
-              org: org || null,
-          };
+            let ipinfo = {
+                ip: ip || null,
+                hostname: hostname || null,
+                city: city || null,
+                region: region || null,
+                country: country || null,
+                loc: loc || null,
+                org: org || null,
+            };
 
-          //verfica se tem parametros na URL
-          const urlParams = new URLSearchParams(window.location.search);
-          if (urlParams.has('af') && urlParams.has('oid')) {
+            //verfica se tem parametros na URL
+            const urlParams = new URLSearchParams(window.location.search);
+            if (urlParams.has('af') && urlParams.has('oid')) {
 
-              for (const [key, value] of urlParams.entries()) {
-                  ipinfo[key] = value;
-              }
+                for (const [key, value] of urlParams.entries()) {
+                    ipinfo[key] = value;
+                }
 
-          } else {
-              ipinfo['af'] = 'Vv5P88AWTr7qsU8v8';
-              ipinfo['bid'] = '23';
-              ipinfo['oid'] = '28';
-              ipinfo['cid'] = '645d01bc3981320001f44bd1';
-          }
+            } else {
+                ipinfo['af'] = 'Vv5P88AWTr7qsU8v8';
+                ipinfo['bid'] = '23';
+                ipinfo['oid'] = '28';
+                ipinfo['cid'] = '645d01bc3981320001f44bd1';
+            }
 
-          var expirationDays = latDays || 7;
-          var expirationDate = new Date();
+            var expirationDays = latDays || 7;
+            var expirationDate = new Date();
 
-          expirationDate.setTime(expirationDate.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
+            expirationDate.setTime(expirationDate.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
 
-          document.cookie = "client_origin=" + encodeURIComponent(JSON.stringify(ipinfo)) + "; expires=" + expirationDate.toUTCString() + "; path=/;";
-      })
-      .catch(function (error) {
-          console.log(error);
-      });
+            document.cookie = "client_origin=" + encodeURIComponent(JSON.stringify(ipinfo)) + "; expires=" + expirationDate.toUTCString() + "; path=/;";
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
 
 //showToast
 function showToast(text) {
-  var x = document.getElementById("snackbar");
-  x.className = "show";
-  document.getElementById("snackbar").innerHTML = text;
-  setTimeout(function () { x.className = x.className.replace("show", `${text}`); }, 3000);
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    document.getElementById("snackbar").innerHTML = text;
+    setTimeout(function () { x.className = x.className.replace("show", `${text}`); }, 3000);
+}
+
+//obtem os parametros do afiliado oriondos dos cookies
+function captureAffiliateData() {
+
+    const urlParams = new URLSearchParams(window.location.search);
+
+    let affiliateData = {
+        affiliateCode: urlParams.get('af') || null,
+        source: urlParams.get('source') || null,
+        productId: urlParams.get('pid') || null,
+        vendorId: urlParams.get('vid') || null,
+        offerId: urlParams.get('oid') || null,
+        clickId: urlParams.get('cid') || null,
+        pixelId: urlParams.get('afx') || null,
+        gtmId: urlParams.get('afgtm') || null,
+        latDays: urlParams.get('latd') || null,
+        brandId: urlParams.get('bid') || null,
+        nextStep: urlParams.get('nxstp') || null,
+        token: urlParams.get('tkn') || null,
+        fbClid: urlParams.get('fbClid') || null,
+        rawUri: window.location.search
+    };
+    return affiliateData;
 }
 
 //registerCustomer
 async function registerCustomer(name, federalId, phone, email, ticket) {
 
-  const button = document.querySelector('.brz-btn-submit');
-  const spinner = button.querySelector('.brz-form-spinner');
-  const span = button.querySelector('.brz-span.brz-text__editor');
+    const affiliate = captureAffiliateData();
 
-  button.setAttribute('disabled', true);
-  spinner.classList.remove('brz-invisible');
-  span.textContent = '';
-  console.log(ticket)
+    const button = document.querySelector('.brz-btn-submit');
+    const spinner = button.querySelector('.brz-form-spinner');
+    const span = button.querySelector('.brz-span.brz-text__editor');
 
-  /* axios.post('https://api2.kemosoft.com.br/api:lp/offer-request-start', { */
-  axios.post('https://api.sheetmonkey.io/form/keboAXgkeWL77ZR39TKRLb', {
-      "name": name,
-      "federalId": federalId,
-      "phone": phone,
-      "email": email,
-      "ticket": ticket
-  },
+    button.setAttribute('disabled', true);
+    spinner.classList.remove('brz-invisible');
+    span.textContent = '';
+    console.log(ticket)
 
-      {
-          headers: {
-              'Access-Control-Allow-Origin': '*',
-              'Content-Type': 'application/json',
-              'X-Client': getCookie('client_origin')
-          }
-      })
-      .then((response) => {
-          window.location.href = 'https://xm16mrwaafp.typeform.com/to/sEzGeuZe';
-      })
+    /* axios.post('https://api2.kemosoft.com.br/api:lp/offer-request-start', { */
+    axios.post('https://api.sheetmonkey.io/form/keboAXgkeWL77ZR39TKRLb', {
+        "name": name,
+        "federalId": federalId,
+        "phone": phone,
+        "email": email,
+        "ticket": ticket,
+        "affiliateData": affiliate
+    },
 
-      .catch(function (error) {
-          button.removeAttribute('disabled');
-          spinner.classList.add('brz-invisible');
-          span.textContent = 'CADASTRAR AGORA!';
-          showToast(error.response.data.message);
-      });
+        {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json',
+                'X-Client': getCookie('client_origin')
+            }
+        })
+        .then((response) => {
+            window.location.href = 'https://xm16mrwaafp.typeform.com/to/sEzGeuZe';
+        })
+
+        .catch(function (error) {
+            button.removeAttribute('disabled');
+            spinner.classList.add('brz-invisible');
+            span.textContent = 'CADASTRAR AGORA!';
+            showToast(error.response.data.message);
+        });
 }
 
 
 const buttonSubmit = document.querySelector(".brz-btn-submit");
 buttonSubmit.addEventListener("click", function (event) {
-  validateForm();
+    validateForm();
 });
 
 
@@ -122,17 +149,17 @@ buttonSubmit.addEventListener("click", function (event) {
 //validar form
 function validateForm() {
 
-  const name = document.querySelector('[data-label="Nome"]').value;
-  const phone = document.querySelector('[data-label="Whatsapp"]').value;
-  const email = document.querySelector('[data-label="Email"]').value;
-  const federalId = document.querySelector('[data-label="CPF"]').value;
+    const name = document.querySelector('[data-label="Nome"]').value;
+    const phone = document.querySelector('[data-label="Whatsapp"]').value;
+    const email = document.querySelector('[data-label="Email"]').value;
+    const federalId = document.querySelector('[data-label="CPF"]').value;
 
-  if (name == "" || federalId == "" || phone == "" || email == "") {
-      showToast("Por favor, preencha todos os campos.");
-      return false;
-  }
+    if (name == "" || federalId == "" || phone == "" || email == "") {
+        showToast("Por favor, preencha todos os campos.");
+        return false;
+    }
 
- registerCustomer(name, federalId, phone, email, ticket);
+    registerCustomer(name, federalId, phone, email, ticket);
 }
 
 
