3bd4a6ccffa0a9d744c683f7c0c97cbadfb26162
Vitor Almeida <vitorarj@ufrn.edu.br>
2023-09-19 14:44:51 -0300

    feat: params

--- translate-crm-pt-br.js
+++ translate-crm-pt-br.js
@@ -1,4 +1,5 @@
 
+//Injeta tradução dos cards do dashboard
 function translateCards(){
 
   const translates = [
@@ -13,104 +14,52 @@ function translateCards(){
     const cardsTitle = document.querySelectorAll('.card-title');
 
     for (const index in cardsTitle) { 
-    
       const text = translates[index];
       const cardDiv = document.querySelectorAll('.card-title')[index];
       cardDiv.textContent = text;
-
     }
 
-}
-
-
-
-
-const traducoes = {
-  "h1": { 
-    "Agency Dashboard": "Painel da Agência"
-  },
-  "h4": {
-    "No Response" : "Sem resposta",
-    "Busy":"Ocupado",
-    "Voicemail" : "Caixa de correio de voz",
-    "Completed" : "Concluído", 
-  },
-  "h5": {
-    "Schedule appointment": "Agendar consulta",
-    "How was the call?" : "Como foi a ligação?", 
-  },
-  "a": {
-    "Agency Dashboard" : "Painel da Agência",
-    "Prospecting" : "Prospecção",
-    "Subaccounts" : "Subcontas",
-    "Account Snapshots" : "Instantâneos de conta",
-    "Resell" : "Revenda",
-    "Online Marketplace" : "Mercado online",
-    "Affiliate Portal": "Portal de Afiliados",
-    "Partners" : "Parceiros",
-    "University" : "Universidade",
-    "SaaS Education" : "Educação SaaS",
-    "Ideas" : "Ideias",
-    "New Mobile App" : "Mobile App New",
-    "Connect WhatsApp": "Conectar WhatsApp",
-    "Settings": "Configurações",
-    "View Changelog": "Ver histórico de alterações",
-    "Notifications": "Notificações",
-    "Support": "Suporte",
-    "Signout": "Sair",
-    "Login to Stripe": "Entrar no Stripe",
-    "Why are my stats inaccurate?": "Por que minhas estatísticas estão imprecisas?"
-  },
-  "span": {
-    "Click here to switch": "Clique aqui para alternar",
-    "Agency Dashboard": "Painel da Agência",
-    "Prospecting": "Prospecção",
-    "New": "Novo",
-    "Subaccounts": "Subcontas",
-    "Instantâneos de conta": "Instantâneos de conta",
-    "Resell": "Revenda",
-    "Online Marketplace": "Mercado online",
-    "Affiliate Portal": "Portal de Afiliados",
-    "Partners": "Parceiros",
-    "University": "Universidade",
-    "SaaS Education": "Educação SaaS",
-    "Ideas": "Ideias",
-    "Mobile App": "Aplicativo Móvel",
-    "Connect WhatsApp": "Conectar WhatsApp",
-    "Settings": "Configurações",
-    "View Changelog": "Ver histórico de alterações",
-    "Notifications": "Notificações",
-    "Support": "Suporte",
-    "Learn More": "Saiba mais",
-    "Use setting": "Usar configuração"
-  }
 };
 
-// Função para substituir o texto de acordo com as traduções
-function traduzirTexto() {
-  translateCards();
-    // Loop através das tags e seus textos
-    for (const tag in traducoes) {
-        const elementos = document.querySelectorAll(tag);
-        elementos.forEach(elemento => {
-            const textoOriginal = elemento.textContent.trim();
-            if (traducoes[tag][textoOriginal]) {
-                elemento.textContent = traducoes[tag][textoOriginal];
-            }
-        });
+
+//Injeta tradução dos links do nav
+function translateNav(){
+
+  const translates = [
+    "Painel da Agência",
+    "Prospecção",
+    "Subcontas",
+    "Capturas de Conta",
+    "Revenda",
+    "Mercado",
+    "Portal de Afiliados",
+    "Parceiros",
+    "Universidade",
+    "Educação SaaS",
+    "Ideas",
+    "Aplicativo Móvel",
+    "Conectar WhatsApp"
+  ];
+
+    const navTitles = document.querySelectorAll('a .nav-title');
+
+    for (const index in navTitles) { 
+      const text = translates[index];
+      const nav = document.querySelectorAll('a .nav-title')[index];
+      nav.textContent = text;
     }
-}
+
+};
+
 
 
 /*  setTimeout(applyTranslate, 2000); */
-
-
 function handlePathChange() {
     const newPath = window.location.pathname;
 
     console.log(`O path da URL foi alterado para: ${newPath}`);
-    setTimeout(traduzirTexto, 4000);
-    console.log('test');
+    setTimeout(traduzirTexto, 3000);
+    console.log('foi teste');
 }
 
 window.addEventListener('popstate', handlePathChange);
