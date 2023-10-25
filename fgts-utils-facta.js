1a35f712c15874d174d5f7fe2d8cad9168a81e72
João Carlos <56844688+joaozeira@users.noreply.github.com>
2023-10-25 15:35:15 -0300

    Update fgts-utils-facta.js

--- fgts-utils-facta.js
+++ fgts-utils-facta.js
@@ -434,80 +434,96 @@ function getNextStep() {
 
 // Qualifica o lead
 function processQualification() {
-    // Recupere os valores do localStorage, se existirem
-    let attempts = localStorage.getItem('attempts') || 0;
-    let attemptsAuth = localStorage.getItem('attemptsAuth') || 0;
-    let minimize = localStorage.getItem('minimize') || false;
-    let attemptsCatch = localStorage.getItem('attemptsCatch') || 0;
-    let pathName = localStorage.getItem('pathName') || null
+  let attempts = localStorage.getItem("attempts") || 0;
+  let attemptsAuth = localStorage.getItem("attemptsAuth") || 0;
+  let attemptsCatch = localStorage.getItem("attemptsCatch") || 0;
 
-    const button = document.querySelector('.brz-btn-submit');
-    const spinner = button.querySelector('.brz-form-spinner');
-    const span = button.querySelector('.brz-span.brz-text__editor');
+  let minimize = localStorage.getItem("minimize") || false;
 
-    button.setAttribute('disabled', true);
-    spinner.classList.remove('brz-invisible');
-    span.textContent = '';
+  const button = document.querySelector(".brz-btn-submit");
+  const spinner = button.querySelector(".brz-form-spinner");
+  const span = button.querySelector(".brz-span.brz-text__editor");
 
-    // Função para enviar a solicitação
-    const sendRequest = () => {
-        axios.post(apiBaseUrl + '/registerCustomerInfos', {
-            enable: true,
-            authorize: true,
-            currentStep: getCurrentStep()
-        }, {
-            headers: {
-                'Authorization': `${getCookie('tkn')}`
+  button.setAttribute("disabled", true);
+  spinner.classList.remove("brz-invisible");
+  span.textContent = "";
+
+  const sendRequest = () => {
+    axios
+      .post(
+        apiBaseUrl + "/registerCustomerInfos",
+        {
+          enable: true,
+          authorize: true,
+          currentStep: getCurrentStep(),
+        },
+        {
+          headers: {
+            Authorization: `${getCookie("tkn")}`,
+          },
+        }
+      )
+      .then((response) => {
+        var switchNextStep = response.data.nextStep;
+        console.log(switchNextStep);
+
+        switch (switchNextStep) {
+          case "enable":
+            console.log("case enable");
+            getNextStep(response.data.nextStep);
+            attempts++;
+
+            localStorage.setItem("attempts", attempts);
+            break;
+          case "authorize":
+            console.log("case authorize");
+            getNextStep(response.data.nextStep);
+            attemptsAuth++;
+
+            localStorage.setItem("attemptsAuth", attemptsAuth);
+            break;
+          case "keepcalm":
+            console.log("case keepcalm");
+
+            attemptsCatch++;
+            if (attemptsCatch < 2) {
+              sendRequest();
+            } else {
+              window.location.href = stepsUrl + "offline";
             }
-        })
-            .then((response) => {
-                switch (pathName) {
-                    case '/enable':
-                        getNextStep();
-                        attemptsCatch = 2;
-                        attempts++;
 
-                        localStorage.setItem('attempts', attempts);
-                        localStorage.setItem('attemptsCatch', attemptsCatch);
-                        break;
-                    case '/authorize':
-                        getNextStep();
-                        attemptsCatch = 2;
-                        attemptsAuth++;
+            localStorage.setItem("attemptsCatch", attemptsCatch);
+            break;
+          default:
+            console.log("default");
+            getNextStep(response.data.nextStep);
+            attempts++;
+            attemptsAuth++;
 
-                        localStorage.setItem('attemptsAuth', attemptsAuth);
-                        localStorage.setItem('attemptsCatch', attemptsCatch);
-                        break;
-                    default:
-                        getNextStep();
-                        attemptsCatch = 2;
-                        attempts++;
-                        attemptsAuth++;
+            localStorage.setItem("attempts", attempts);
+            localStorage.setItem("attemptsAuth", attemptsAuth);
+            break;
+        }
+      })
+      .catch(function (error) {
+        attemptsCatch++;
+        if (attemptsCatch < 2) {
+          sendRequest();
+        } else {
+          window.location.href = stepsUrl + "offline";
+        }
+      });
+  };
 
-                        localStorage.setItem('attempts', attempts);
-                        localStorage.setItem('attemptsAuth', attemptsAuth);
-                        localStorage.setItem('attemptsCatch', attemptsCatch);
-                        break;
-                }
-            })
-            .catch(function (error) {
-                attemptsCatch++;
-                if (attemptsCatch < 2) {
-                    sendRequest();
-                } else {
-                    window.location.href = stepsUrl + 'offline';
-                }
-            });
-    }
+  sendRequest();
 
-    sendRequest();
-
-    // Salve os valores finais no localStorage
-    localStorage.setItem('attempts', attempts);
-    localStorage.setItem('attemptsAuth', attemptsAuth);
-    localStorage.setItem('minimize', minimize);
-    localStorage.setItem('attemptsCatch', attemptsCatch);
+  // Salve os valores finais no localStorage
+  localStorage.setItem("attempts", attempts);
+  localStorage.setItem("attemptsAuth", attemptsAuth);
+  localStorage.setItem("minimize", minimize);
+  localStorage.setItem("attemptsCatch", attemptsCatch);
 }
+
 
 /* function processQualification() {
 
