78a8c6c47050979138043a5372475f7e2f1c7498
João Carlos <56844688+joaozeira@users.noreply.github.com>
2024-02-06 09:46:23 -0300

    Update consigmaisFGTS-utils.js

--- consigmaisFGTS-utils.js
+++ consigmaisFGTS-utils.js
@@ -558,7 +558,7 @@ function qualification() {
 
                 switch (contexto) {
                     //REQUER AÇÃO DO CLIENTE
-                    case "resolver-situação":
+                    case "resolver-situacao":
                         switch (situacao) {
                             case "habilitar-saque":
                                 URL_redirect = `/enable`;
