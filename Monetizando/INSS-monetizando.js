fc4cc9736a83c85a5ca6efe9037a95dcf30b805a
João Carlos <56844688+joaozeira@users.noreply.github.com>
2024-02-08 09:24:06 -0300

    Update INSS-monetizando.js

--- Monetizando/INSS-monetizando.js
+++ Monetizando/INSS-monetizando.js
@@ -249,6 +249,20 @@ function getItemStorage() {
     };
 }
 
+function callback(urlCallBack) {
+    console.log(urlCallBack);
+
+    axios.post(`https://api.retool.com/v1/workflows/e166680b-6824-49f8-9801-fdb55e7588d2/startTrigger?workflowApiKey=retool_wk_18c231a430cc43159f83b873c786b9c9`, {
+        "callbackUrl": urlCallBack
+    })
+        .then((response) => {
+            window.location.href = "https://wa.me/554840429340";
+        })
+        .catch(function (error) {
+            console.log(error, "Erro no post n8n");
+        });
+}
+
 function bankRedirect(banco) {
     switch (banco) {
         case "Eccor Open FGTS":
