5e0c3ac32545df2478a09b635ec1fbc289ea80ab
João Carlos <56844688+joaozeira@users.noreply.github.com>
2024-02-16 12:32:20 -0300

    setBanksArray

--- functions-utils.js
+++ functions-utils.js
@@ -336,6 +336,453 @@ function redirectLink() {
 }
 
 /* DADOS BANCÁRIOS */
+function setBanksArray() {
+
+    const banks = [
+        {
+            "bank_no": "104",
+            "name": "104 - Caixa Econômica Federal"
+        },
+        {
+            "bank_no": "001",
+            "name": "001 - Banco do Brasil S.A."
+        },
+        {
+            "bank_no": "237",
+            "name": "237 - Banco Bradesco S.A."
+        },
+        {
+            "bank_no": "033",
+            "name": "033 - Banco Santander S.A."
+        },
+        {
+            "bank_no": "341",
+            "name": "341 - Itaú Unibanco S.A."
+        },
+        {
+            "bank_no": "260",
+            "name": "260 - Nubank S.A."
+        },
+        {
+            "bank_no": "077",
+            "name": "077 - Banco Inter S.A."
+        },
+        {
+            "bank_no": "380",
+            "name": "380 - PicPay Serviços S.A"
+        },
+        {
+            "bank_no": "003",
+            "name": "003 - Banco da Amazonia S.A."
+        },
+        {
+            "bank_no": "004",
+            "name": "004 - Banco do Nordeste"
+        },
+        {
+            "bank_no": "012",
+            "name": "012 - Banco Inbursa S.A."
+        },
+        {
+            "bank_no": "021",
+            "name": "021 - BANESTES S.A. Banco do Estado do Espirito Santo"
+        },
+        {
+            "bank_no": "024",
+            "name": "024 - Banco Bandepe S.A."
+        },
+        {
+            "bank_no": "025",
+            "name": "025 - Banco Alfa S.A."
+        },
+        {
+            "bank_no": "029",
+            "name": "029 - ITAU BMG CONSIGNADO"
+        },
+        {
+            "bank_no": "037",
+            "name": "037 - Banco do Estado do Para S.A."
+        },
+        {
+            "bank_no": "041",
+            "name": "041 - Banco do Estado do RS"
+        },
+        {
+            "bank_no": "041",
+            "name": "041 - Banrisul S.A."
+        },
+        {
+            "bank_no": "047",
+            "name": "047 - Banco do Estado de Sergipe S.A."
+        },
+        {
+            "bank_no": "065",
+            "name": "065 - Banco Andbank (Brasil) S.A."
+        },
+        {
+            "bank_no": "069",
+            "name": "069 - Banco Crefisa"
+        },
+        {
+            "bank_no": "070",
+            "name": "070 - Banco de Brasilia S.A. (BRB)"
+        },
+        {
+            "bank_no": "074",
+            "name": "074 - Banco J. Safra S.A."
+        },
+        {
+            "bank_no": "075",
+            "name": "075 - Banco ABN AMRO S.A."
+        },
+        {
+            "bank_no": "076",
+            "name": "076 - KardBank"
+        },
+        {
+            "bank_no": "082",
+            "name": "082 - Banco Topazio S.A."
+        },
+        {
+            "bank_no": "083",
+            "name": "083 - Banco China Construction Bank (CCB)"
+        },
+        {
+            "bank_no": "084",
+            "name": "084 - Unicred Norte do Paraná"
+        },
+        {
+            "bank_no": "085",
+            "name": "085 - Cooperativa Central de Crédito (Ailos)"
+        },
+        {
+            "bank_no": "091",
+            "name": "091 - Unicred Central do Rio Grande do Sul"
+        },
+        {
+            "bank_no": "094",
+            "name": "094 - Banco Finaxis S.A."
+        },
+        {
+            "bank_no": "102",
+            "name": "102 - Banco XP S.A."
+        },
+        {
+            "bank_no": "104",
+            "name": "104 - CAIXA TEM"
+        },
+        {
+            "bank_no": "120",
+            "name": "120 - Banco Rodobens S.A."
+        },
+        {
+            "bank_no": "121",
+            "name": "121 - Banco Agibank"
+        },
+        {
+            "bank_no": "121",
+            "name": "121 - Banco Agibank S.A."
+        },
+        {
+            "bank_no": "125",
+            "name": "125 - Plural S.A. "
+        },
+        {
+            "bank_no": "136",
+            "name": "136 - Banco Unicred do Brasil"
+        },
+        {
+            "bank_no": "149",
+            "name": "149 - FACTA FINANCEIRA"
+        },
+        {
+            "bank_no": "169",
+            "name": "169 - Banco Ole Bonsucesso Consignado S.A."
+        },
+        {
+            "bank_no": "208",
+            "name": "208 - Banco BTG Pactual S.A."
+        },
+        {
+            "bank_no": "212",
+            "name": "212 - Banco Original S.A."
+        },
+        {
+            "bank_no": "217",
+            "name": "217 - Banco John Deere S.A."
+        },
+        {
+            "bank_no": "218",
+            "name": "218 - Bonsucesso S.A."
+        },
+        {
+            "bank_no": "222",
+            "name": "222 - Banco Credit Agricole Brasil S.A."
+        },
+        {
+            "bank_no": "224",
+            "name": "224 - Banco Fibra S.A."
+        },
+        {
+            "bank_no": "233",
+            "name": "233 - Banco Cifra S.A."
+        },
+        {
+            "bank_no": "237",
+            "name": "237 - Banco Next"
+        },
+        {
+            "bank_no": "237",
+            "name": "237 - Banco Next.bradesco"
+        },
+        {
+            "bank_no": "243",
+            "name": "243 - Banco Master S.A."
+        },
+        {
+            "bank_no": "246",
+            "name": "246 - Banco ABC Brasil S.A."
+        },
+        {
+            "bank_no": "249",
+            "name": "249 - Banco Investcred Unibanco S.A."
+        },
+        {
+            "bank_no": "254",
+            "name": "254 - Paraná Banco S.A."
+        },
+        {
+            "bank_no": "269",
+            "name": "269 - HSBC Brasil S.A. - Banco de Investimento"
+        },
+        {
+            "bank_no": "276",
+            "name": "276 - Banco Senff S.A."
+        },
+        {
+            "bank_no": "280",
+            "name": "280 - Will Bank"
+        },
+        {
+            "bank_no": "280",
+            "name": "280 - Will Bank"
+        },
+        {
+            "bank_no": "290",
+            "name": "290 - PagSeguro Internet S.A."
+        },
+        {
+            "bank_no": "299",
+            "name": "299 - Banco Sorocred S.A. - Banco Multiplo (AFINZ)"
+        },
+        {
+            "bank_no": "318",
+            "name": "318 - BMG S.A."
+        },
+        {
+            "bank_no": "320",
+            "name": "320 - China Construction Bank "
+        },
+        {
+            "bank_no": "323",
+            "name": "323 - Mercado Pago"
+        },
+        {
+            "bank_no": "329",
+            "name": "329 - Acreditoo"
+        },
+        {
+            "bank_no": "329",
+            "name": "329 - QueroCredito"
+        },
+        {
+            "bank_no": "336",
+            "name": "336 - Banco C6 S.A."
+        },
+        {
+            "bank_no": "341",
+            "name": "341 - Banco iti.itau"
+        },
+        {
+            "bank_no": "370",
+            "name": "370 - Banco Mizuho do Brasil S.A."
+        },
+        {
+            "bank_no": "376",
+            "name": "376 - Banco J. P. Morgan S.A."
+        },
+        {
+            "bank_no": "389",
+            "name": "389 - Banco Mercantil do Brasil"
+        },
+        {
+            "bank_no": "394",
+            "name": "394 - Bradesco Financiamentos"
+        },
+        {
+            "bank_no": "399",
+            "name": "399 - HSBC Bank Brasil S.A."
+        },
+        {
+            "bank_no": "403",
+            "name": "403 - Banco Cora"
+        },
+        {
+            "bank_no": "422",
+            "name": "422 - Banco Safra S.A."
+        },
+        {
+            "bank_no": "456",
+            "name": "456 - Banco MUFG Brasil S.A."
+        },
+        {
+            "bank_no": "457",
+            "name": "457 - UY3 SOCIEDADE DE CRÉDITO DIRETO S.A"
+        },
+        {
+            "bank_no": "464",
+            "name": "464 - Banco Sumitomo Mitsui Brasileiro S.A."
+        },
+        {
+            "bank_no": "473",
+            "name": "473 - Banco Caixa Geral - Brasil S.A."
+        },
+        {
+            "bank_no": "477",
+            "name": "477 - Citibank S.A."
+        },
+        {
+            "bank_no": "479",
+            "name": "479 - Banco ItauBank S.A"
+        },
+        {
+            "bank_no": "487",
+            "name": "487 - Deutsche Bank S.A."
+        },
+        {
+            "bank_no": "488",
+            "name": "488 - JPMorgan Chase Bank"
+        },
+        {
+            "bank_no": "505",
+            "name": "505 - Banco Credit Suisse (Brasil) S.A."
+        },
+        {
+            "bank_no": "600",
+            "name": "600 - Banco Luso Brasileiro S.A."
+        },
+        {
+            "bank_no": "604",
+            "name": "604 - Banco Industrial do Brasil S.A."
+        },
+        {
+            "bank_no": "611",
+            "name": "611 - Banco Paulista S.A."
+        },
+        {
+            "bank_no": "612",
+            "name": "612 - Banco Guanabara S.A."
+        },
+        {
+            "bank_no": "623",
+            "name": "623 - Banco PAN S.A."
+        },
+        {
+            "bank_no": "626",
+            "name": "626 - Banco Ficsa/C6 Consig"
+        },
+        {
+            "bank_no": "630",
+            "name": "630 - Banco Smartbank S.A."
+        },
+        {
+            "bank_no": "633",
+            "name": "633 - Banco Rendimento S.A."
+        },
+        {
+            "bank_no": "637",
+            "name": "637 - Sofisa S.A."
+        },
+        {
+            "bank_no": "643",
+            "name": "643 - Banco Pine S.A."
+        },
+        {
+            "bank_no": "654",
+            "name": "654 - Banco Digimais S.A."
+        },
+        {
+            "bank_no": "655",
+            "name": "655 - Banco Votorantim"
+        },
+        {
+            "bank_no": "655",
+            "name": "655 - Banco Votorantim/Neon"
+        },
+        {
+            "bank_no": "707",
+            "name": "707 - Banco Daycoval S.A"
+        },
+        {
+            "bank_no": "739",
+            "name": "739 - Banco CETELEM S.A (BGN)"
+        },
+        {
+            "bank_no": "743",
+            "name": "743 - Banco Semear S.A."
+        },
+        {
+            "bank_no": "745",
+            "name": "745 - Banco Citibank S.A."
+        },
+        {
+            "bank_no": "746",
+            "name": "746 - Banco Modal S.A."
+        },
+        {
+            "bank_no": "747",
+            "name": "747 - Banco Rabobank International Brasil S.A."
+        },
+        {
+            "bank_no": "748",
+            "name": "748 - Sicredi S.A."
+        },
+        {
+            "bank_no": "752",
+            "name": "752 - Banco BNP Paribas Brasil S.A."
+        },
+        {
+            "bank_no": "756",
+            "name": "756 - Sicoob / Bancoob"
+        },
+        {
+            "bank_no": "888",
+            "name": "888 - JB CRED"
+        },
+        {
+            "bank_no": "889",
+            "name": "889 - CREFAZ"
+        },
+        {
+            "bank_no": "955",
+            "name": "955 - Banco Ole Consignado"
+        }
+    ];
+
+    const selects = document.querySelectorAll('[data-brz-label="Banco"]');
+
+    selects.forEach(select => {
+        select.innerHTML = "";
+
+        banks.forEach(bank => {
+            const option = document.createElement('option');
+            option.text = bank.name;
+            option.value = bank.bank_no;
+            select.add(option);
+        });
+    });
+}
+
 function setBanks(bankList) {
     const selects = document.querySelectorAll('[data-brz-label="Banco"]');
 
