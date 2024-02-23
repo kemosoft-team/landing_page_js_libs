bafc1c924da3ed073b4a7e766b32148b6219122e
João Carlos <56844688+joaozeira@users.noreply.github.com>
2024-02-23 12:02:35 -0300

    Update testFGTS.js

--- testFGTS.js
+++ testFGTS.js
@@ -55,7 +55,7 @@ function validatorQuestions() {
         showToast("Por favor, responda todas as perguntas.");
         return false;
 
-    } else if (estiloComputado.display === 'none') {
+    } else if ((firstChoice === "Sim, estou trabalhando com carteira assinada." || firstChoice === "Sim, já trabalhei assim antes, mas não estou mais.") && estiloComputado.display === 'none') {
         showToast('Por favor, autorize os bancos e marque a caixa acima.');
         return false;
 
