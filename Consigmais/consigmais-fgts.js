2e6eecec7c59fa8c08b439e3e815a5a87f743e69
João Carlos <56844688+joaozeira@users.noreply.github.com>
2024-03-12 10:18:58 -0300

    remover change questions 2

--- Consigmais/consigmais-fgts.js
+++ Consigmais/consigmais-fgts.js
@@ -393,21 +393,4 @@ function changeQuestionOne() {
         }
     }
 }
-function changeQuestionTwo() {
-    var selectedOption = selectElement2.options[selectElement2.selectedIndex].value;
-    if (selectedOption === "Sim, já está ativado.") {
-        var divs = forms2Element.querySelectorAll(".brz-forms2__item");
-        if (divs.length >= 2) {
-            divs[2].style.display = "block";
-            textFooter.style.display = "block";
-            textBanksDiv.style.display = "block";
-        }
-    } else {
-        var divs = forms2Element.querySelectorAll(".brz-forms2__item");
-        if (divs.length >= 2) {
-            divs[2].style.display = "none";
-            textFooter.style.display = "none";
-            textBanksDiv.style.display = "none";
-        }
-    }
-}
+
