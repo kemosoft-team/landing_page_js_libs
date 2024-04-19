15012cf54bb527ea8dfee2c6dff1cd6b4688db59
João Carlos <56844688+joaozeira@users.noreply.github.com>
2024-04-19 11:45:39 -0300

    fix open questions

--- Consigmais/consigmais-inss.js
+++ Consigmais/consigmais-inss.js
@@ -168,9 +168,9 @@ function criar_questions() {
             console.log(aposentadoOuPensionista);
             console.log(jaContratouEmprestimo);
             
-            //ABRA O POP UP DE REPRESENTANTE
-            const formRepresentative = document.getElementById("form_representative");
-            formRepresentative.click();
+            // ABRIR QUESTÕES REPRESENTANTE
+            const representativeQuestions = document.getElementById("question_representative");
+            representativeQuestions.click();
         })
         .catch(function (error) {
             showToast(error.response.data.message);
