ab6279a5851ad3dd8df3cd06a85b65dccdd75b11
João Carlos <56844688+joaozeira@users.noreply.github.com>
2024-06-10 16:54:30 -0300

    Update consigmais-fgts.js

--- Consigmais/consigmais-fgts.js
+++ Consigmais/consigmais-fgts.js
@@ -9,6 +9,8 @@ let referrer = document.referrer;
 let name;
 let phone;
 let federalId;
+let birth;
+let email;
 let leadId
 
 let jaTrabalhouCarteiraAssinada;
@@ -67,11 +69,17 @@ function validar_contato_fgts() {
     const federalIdElement = document.querySelector(
         '[data-brz-label="CPF"]'
     ).value;
-
+    const birthElement = document.querySelector(
+        '[data-brz-label="Data de Nascimento"]'
+    ).value;
+    const emailElement = document.querySelector(
+        '[data-brz-label="Email (Opcional)"]'
+    ).value;
     if (
         nameElement == "" ||
         phoneElement == "" ||
-        federalIdElement == "" 
+        federalIdElement == "" ||
+        birthElement == ""
     ) {
         showToast("Por favor, preencha todos os campos.");
         return false;
@@ -84,15 +92,33 @@ function validar_contato_fgts() {
         showToast("O CPF não é válido!");
         return false;
     }
+    if (!isBirthValidFGTS(birthElement)) {
+        var popUpBirth = document.querySelector("#popUpBirth");
+        popUpBirth.click();
+        return false;
+    }
+    if (!isDateValid(birthElement)) {
+        showToast("A data de nascimento informada não é válida!");
+        return false;
+    }
+    if (!isUnderage(birthElement)) {
+        showToast("A data de nascimento informada não é válida!!");
+        return false;
+    }
     if (!validatePhone(phoneElement)) {
         showToast("O número do Whatsapp informado não é válido!");
         return false;
     }
-
+    if (emailElement && !validateEmail(emailElement)) {
+        showToast("O e-mail informado não é válido!");
+        return false;
+    }
     //SALVAR NAS VARIAVEIS GLOBAIS
     name = nameElement;
     phone = phoneElement;
     federalId = federalIdElement;
+    birth = birthElement;
+    email = emailElement
 
     criar_contato_fgts();
 }
@@ -107,6 +133,7 @@ async function criar_contato_fgts() {
     /* REPLACE */
     const federalId_replaced = federalId.replace(/[^\d]/g, "");
     const name_replaced = name.replace(/\s+/g, ' ');
+    const email_replaced = email.replace(/\s/g, "");
 
     const button = document.querySelector(".brz-btn-submit.submit_form");
     const spinner = button.querySelector(".brz-form-spinner");
@@ -119,7 +146,9 @@ async function criar_contato_fgts() {
     axios.post(API_URL + '/v2/criar-contato', {
         nome: name_replaced,
         telefone: phone,
+        dataNascimento: birth,
         cpf: federalId_replaced,
+        email: email_replaced,
         bancosAutorizados: autorizedBanks,
         funil: pipeline_slug,
         urlOrigem: origin,
@@ -132,6 +161,7 @@ async function criar_contato_fgts() {
         .then(async (response) => {
             leadId = response.data.id;
             federalId = federalId_replaced;
+            console.log("Criar Contato: ", leadId)
 
             setItemStorage({
                 pipelineSlug: pipeline_slug,
@@ -142,8 +172,7 @@ async function criar_contato_fgts() {
             const customId = federalId_replaced;
             window.clarity("identify", customId);
 
-            //mautic
-            mt('send', 'pageview', {firstname: name_replaced, phone: phone, funil: pipeline_slug, cpf: federalId_replaced, pageTitle: window.location.pathname});
+            mt('send', 'pageview', { email: email_replaced, firstname: name_replaced, phone: phone });
 
             //ABRA O POP UP DE QUESTIONARIO
             const questions = document.getElementById("questions");
@@ -195,7 +224,7 @@ function criar_questions(jaTrabalhouCarteiraAssinada, saqueHabilitado) {
                 attemptsAuth++;
                 localStorage.setItem("attemptsAuth", attemptsAuth);
                 qualification()
-               /*  window.location.href = "authorize" + "?" + "federalId=" + federalId + "&" + "id=" + leadId; */
+                /*  window.location.href = "authorize" + "?" + "federalId=" + federalId + "&" + "id=" + leadId; */
             }
         })
         .catch(function (error) {
