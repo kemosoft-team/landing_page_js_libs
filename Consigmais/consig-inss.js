667c25efeb73b14c24cf66e79073dd0d3b336bad
João Carlos <56844688+joaozeira@users.noreply.github.com>
2024-06-12 10:31:00 -0300

    alterar numero do callback url

--- Consigmais/consig-inss.js
+++ Consigmais/consig-inss.js
@@ -28,7 +28,7 @@ function callback(urlCallBack) {
         "callbackUrl": urlCallBack
     })
         .then((response) => {
-            window.location.href = "https://wa.me/558440429964";
+            window.location.href = "https://wa.me/558440429946";
         })
         .catch(function (error) {
             console.log(error, "Erro no post n8n");
