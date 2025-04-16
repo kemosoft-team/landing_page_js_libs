4cd5473f2f534315c5d4bfd83de8ca321b67db37
João Carlos <56844688+joaozeira@users.noreply.github.com>
2025-04-16 15:55:34 -0300

    fix: message.error

--- Consigmais/siape.js
+++ Consigmais/siape.js
@@ -220,7 +220,7 @@ async function criar_contato(fullName, whatsapp, federalId) {
     button.removeAttribute("disabled");
     spinner.classList.add("brz-invisible");
     icon.style.display = '';
-    showToast("Ocorreu um erro. Tente novamente.");
+    showToast(error?.response?.data?.message || "Ocorreu um erro. Tente novamente.");
     console.error("Erro ao criar contato:", error);
 
     await logError(
