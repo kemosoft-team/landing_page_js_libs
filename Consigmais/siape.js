ed566df5d843ce8d231d81dc881da89d6209a7a6
João Carlos <56844688+joaozeira@users.noreply.github.com>
2025-04-16 15:31:34 -0300

    Update siape.js

--- Consigmais/siape.js
+++ Consigmais/siape.js
@@ -220,7 +220,7 @@ async function criar_contato(fullName, whatsapp, federalId) {
     button.removeAttribute("disabled");
     spinner.classList.add("brz-invisible");
     icon.style.display = '';
-    showToast(error.message);
+    showToast(response.message);
     console.error("Erro ao criar contato:", error);
 
     await logError(
