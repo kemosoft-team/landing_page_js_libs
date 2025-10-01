6b99c50715aa075c6501f2b94b6dc74810678a10
João Carlos <56844688+joaozeira@users.noreply.github.com>
2025-10-01 16:38:31 -0300

    Update heymax_form.js

--- formularios_heymax/heymax_form.js
+++ formularios_heymax/heymax_form.js
@@ -1,5 +1,5 @@
 // Função para inserir o modal no DOM
-function insertModalHTML(journeyId, urlParams) {
+function insertModalHTML(journeyId, urlParams, formInitial) {
 
 
    
@@ -17,7 +17,7 @@ function insertModalHTML(journeyId, urlParams) {
           width: 100%;
         "
       >
-       <iframe src="https://simule.faz.vc/formFGTS?id=${journeyId}&${urlParams}&urlReferrer=${urlReferrer}"  width="100%" height="100%" frameborder="0"></iframe>
+       <iframe src="https://simule.faz.vc/${formInitial}?id=${journeyId}&${urlParams}&urlReferrer=${urlReferrer}"  width="100%" height="100%" frameborder="0"></iframe>
 
         <div class="btnClose">
           <svg
